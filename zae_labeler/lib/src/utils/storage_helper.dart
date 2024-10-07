e52336d0f7025f686b8956ecb95a57ee690259ed
zae-park <tom941105@gmail.com>
2024-10-07 17:51:30 +0900

    feat: - add import & export features for project configuration

--- zae_labeler/lib/src/utils/storage_helper.dart
+++ zae_labeler/lib/src/utils/storage_helper.dart
@@ -1,6 +1,7 @@
 // lib/src/utils/storage_helper.dart
 import 'dart:convert';
 import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
 import '../models/project_model.dart';
@@ -10,6 +11,43 @@ import '../models/label_entry.dart';
 class StorageHelper {
   static const String projectsKey = 'projects';
   static const String labelsKey = 'labels';
+
+  // 프로젝트 설정 파일 다운로드
+  static Future<String> downloadProjectConfig(Project project) async {
+    // 플랫폼별로 디렉토리 설정
+    Directory directory;
+    if (Platform.isAndroid) {
+      // Android: Downloads 디렉토리
+      final dirs =
+          await getExternalStorageDirectories(type: StorageDirectory.downloads);
+      if (dirs == null || dirs.isEmpty) {
+        throw Exception('다운로드 디렉토리를 찾을 수 없습니다.');
+      }
+      directory = dirs.first;
+    } else if (Platform.isIOS) {
+      // iOS: 애플리케이션 문서 디렉토리
+      directory = await getApplicationDocumentsDirectory();
+    } else if (Platform.isWindows) {
+      // Windows: Downloads 디렉토리
+      final downloadsDirectory = await getDownloadsDirectory();
+      if (downloadsDirectory == null) {
+        throw Exception('Downloads 디렉토리를 찾을 수 없습니다.');
+      }
+      directory = downloadsDirectory;
+    } else {
+      throw UnsupportedError('지원하지 않는 플랫폼입니다.');
+    }
+
+    // 파일 경로 설정
+    String filePath = '${directory.path}\\${project.name}_config.json';
+    File file = File(filePath);
+
+    // 프로젝트를 JSON으로 변환하여 파일에 저장
+    String jsonString = jsonEncode(project.toJson());
+    await file.writeAsString(jsonString);
+
+    return filePath;
+  }
 
   // 프로젝트 저장
   static Future<void> saveProjects(List<Project> projects) async {
