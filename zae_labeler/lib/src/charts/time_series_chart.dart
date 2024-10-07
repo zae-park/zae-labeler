71da8581fc51d4a23c53eefb29e16cfd0033ddcd
zae-park <tom941105@gmail.com>
2024-10-07 17:09:17 +0900

    feat: optimize data chart

--- zae_labeler/lib/src/charts/time_series_chart.dart
+++ zae_labeler/lib/src/charts/time_series_chart.dart
@@ -19,16 +19,74 @@ class TimeSeriesChart extends StatelessWidget {
         LineChartData(
           minY: minY - margin,
           maxY: maxY + margin,
-          gridData: FlGridData(show: true),
+          gridData: FlGridData(
+            show: true,
+            // drawVerticalLine: false, // 수직 Grid Line 비활성화
+            getDrawingHorizontalLine: (value) {
+              if (value == 0) {
+                return FlLine(
+                  color: Colors.grey,
+                  strokeWidth: 1.5,
+                );
+              }
+              return FlLine(
+                color: Colors.grey,
+                strokeWidth: 0.8,
+                dashArray: [5, 5],
+              );
+            },
+          ),
           titlesData: FlTitlesData(
+            show: true,
+            topTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
             leftTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: true),
+              sideTitles: SideTitles(showTitles: false),
             ),
             bottomTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: true),
+              sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 32,
+                interval: (maxY + margin - (minY - margin)) / 10, // 적절한 간격 설정
+                getTitlesWidget: (value, meta) {
+                  return SideTitleWidget(
+                    axisSide: meta.axisSide,
+                    child: Text(
+                      value.toStringAsFixed(1),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            rightTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 40,
+                interval: (maxY + margin - (minY - margin)) / 5, // 적절한 간격 설정
+                getTitlesWidget: (value, meta) {
+                  return SideTitleWidget(
+                    axisSide: meta.axisSide,
+                    child: Text(
+                      value.toStringAsFixed(1),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ),
-          borderData: FlBorderData(show: true),
+          borderData: FlBorderData(
+            show: true,
+            border: Border.all(color: Colors.grey),
+          ),
           lineBarsData: [
             LineChartBarData(
               spots: generateSpots(data),
