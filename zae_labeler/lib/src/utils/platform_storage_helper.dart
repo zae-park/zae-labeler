d4de937810fcd8a3807894284ba5950b478dd9e0
zae-park <tom941105@gmail.com>
2024-11-16 00:08:32 +0900

    feat: split platform depend implementations

---
+++ zae_labeler/lib/src/utils/platform_storage_helper.dart
@@ -0,0 +1,16 @@
+// lib/src/utils/platform_storage_helper.dart
+import 'dart:io';
+
+import '../models/project_model.dart';
+import '../models/label_entry.dart';
+
+abstract class PlatformStorageHelper {
+  Future<String> downloadProjectConfig(Project project);
+  Future<List<Project>> loadProjects();
+  Future<void> saveProjects(List<Project> projects);
+  Future<List<LabelEntry>> loadLabelEntries();
+  Future<void> saveLabelEntries(List<LabelEntry> labelEntries);
+  Future<String> downloadLabelsAsZip(
+      Project project, List<LabelEntry> labelEntries, List<File> dataFiles);
+  Future<List<LabelEntry>> importLabelEntries();
+}
