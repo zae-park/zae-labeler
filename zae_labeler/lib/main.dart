74a8dbae8a4498751f7515690c73ea8c49fe8913
zae-park <tom941105@gmail.com>
2024-11-27 18:40:02 +0900

    feat: apply lang files

--- zae_labeler/lib/main.dart
+++ zae_labeler/lib/main.dart
@@ -13,28 +13,57 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Root widget of the application
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      // Registering providers for state management.
-      //Add other providers here if needed in the future
-      providers: [
-        ChangeNotifierProvider<ProjectViewModel>(
-          create: (_) => ProjectViewModel(),
-        ),
-      ],
-      child: MaterialApp(
-        // Application title displayed in task switcher or web browser tab
-        title: 'Data Labeling App for YOU !',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        initialRoute: '/', // Initial route when the app is launched
-        routes: {
-          '/': (context) => const ProjectListPage(),
-          '/configuration': (context) => const ConfigureProjectPage(),
-          '/labeling': (context) => const LabelingPage(),
+    return ChangeNotifierProvider(
+      create: (_) => LocaleViewModel(),
+      child: Consumer<LocaleViewModel>(
+        builder: (context, localeVM, child) {
+          return MaterialApp(
+            title: 'Data Labeling App',
+            theme: ThemeData(primarySwatch: Colors.blue),
+            locale: localeVM.currentLocale,
+            supportedLocales: const [
+              Locale('en'), // English
+              Locale('ko') // Korean
+            ],
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const ProjectListPage(),
+            },
+          );
         },
       ),
     );
   }
+
+  // // Root widget of the application
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MultiProvider(
+  //     // Registering providers for state management.
+  //     //Add other providers here if needed in the future
+  //     providers: [
+  //       ChangeNotifierProvider<ProjectViewModel>(
+  //         create: (_) => ProjectViewModel(),
+  //       ),
+  //     ],
+  //     child: MaterialApp(
+  //       // Application title displayed in task switcher or web browser tab
+  //       title: 'Data Labeling App for YOU !',
+  //       theme: ThemeData(primarySwatch: Colors.blue),
+  //       initialRoute: '/', // Initial route when the app is launched
+  //       routes: {
+  //         '/': (context) => const ProjectListPage(),
+  //         '/configuration': (context) => const ConfigureProjectPage(),
+  //         '/labeling': (context) => const LabelingPage(),
+  //       },
+  //     ),
+  //   );
+  // }
 }
