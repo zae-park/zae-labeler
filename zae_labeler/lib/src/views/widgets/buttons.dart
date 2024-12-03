3e01d5d1c331f1184468fedf596df257ad83fb90
zae-park <tom941105@gmail.com>
2024-12-03 15:43:58 +0900

    feat: add AppButton class

--- zae_labeler/lib/src/views/widgets/buttons.dart
+++ zae_labeler/lib/src/views/widgets/buttons.dart
@@ -1,17 +1,34 @@
-// lib/src/widgets/buttons.dart
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  final String label;
+class AppButton extends StatelessWidget {
+  final IconData? icon;
+  final String? label;
   final VoidCallback onPressed;
 
-  const CustomButton({super.key, required this.label, required this.onPressed});
+  const AppButton({super.key, this.icon, this.label, required this.onPressed});
+
+  /// Factory for a home icon button
+  factory AppButton.home({required VoidCallback onPressed}) {
+    return AppButton(icon: Icons.home, onPressed: onPressed);
+  }
+
+  /// Factory for a settings icon button
+  factory AppButton.settings({required VoidCallback onPressed}) {
+    return AppButton(icon: Icons.settings, onPressed: onPressed);
+  }
+
+  /// Factory for a save icon button
+  factory AppButton.save({required VoidCallback onPressed}) {
+    return AppButton(icon: Icons.save, onPressed: onPressed);
+  }
+
+  /// Factory for a cancel icon button
+  factory AppButton.cancel({required VoidCallback onPressed}) {
+    return AppButton(icon: Icons.cancel, onPressed: onPressed);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(label),
-    );
+    return IconButton(icon: Icon(icon), tooltip: label, onPressed: onPressed);
   }
 }
