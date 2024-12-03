9e926a32521d91913d3f818f58ad9ab72c66b991
zae-park <tom941105@gmail.com>
2024-12-03 23:45:09 +0900

    feat: replace buttons & lint factories

---
+++ zae_labeler/lib/src/views/widgets/core/buttons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final IconData? icon;
+  final String? label;
+  final VoidCallback onPressed;
+
+  const AppButton({super.key, this.icon, this.label, required this.onPressed});
+
+  /// Factory for a home icon button
+  factory AppButton.home({required VoidCallback onPressed}) => AppButton(icon: Icons.home, onPressed: onPressed);
+
+  factory AppButton.settings({required VoidCallback onPressed}) => AppButton(icon: Icons.settings, onPressed: onPressed);
+  factory AppButton.save({required VoidCallback onPressed}) => AppButton(icon: Icons.save, onPressed: onPressed);
+  factory AppButton.cancel({required VoidCallback onPressed}) => AppButton(icon: Icons.cancel, onPressed: onPressed);
+
+  @override
+  Widget build(BuildContext context) => IconButton(icon: Icon(icon), tooltip: label, onPressed: onPressed);
+}
