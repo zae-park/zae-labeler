7aed0460d2076efb6cf29058941204dcc04851f3
zae-park <tom941105@gmail.com>
2024-12-11 15:15:41 +0900

    feat: add FileData model

--- zae_labeler/lib/src/models/data_model.dart
+++ zae_labeler/lib/src/models/data_model.dart
@@ -8,5 +8,27 @@ class UnifiedData {
   final Map<String, dynamic>? objectData; // JSON 오브젝트 데이터
   final FileType fileType; // 파일 유형
 
-  UnifiedData({this.file, this.seriesData, this.objectData, required this.fileType});
+  UnifiedData(
+      {this.file, this.seriesData, this.objectData, required this.fileType});
+}
+
+class FileData {
+  final String name; // 파일 이름
+  final String type; // 파일 타입 (e.g., 'image/png', 'application/json')
+  final String content; // 파일 내용 (base64 인코딩)
+
+  FileData({required this.name, required this.type, required this.content});
+
+  // JSON 변환 메소드
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'type': type,
+        'content': content,
+      };
+
+  static FileData fromJson(Map<String, dynamic> json) => FileData(
+        name: json['name'],
+        type: json['type'],
+        content: json['content'],
+      );
 }
