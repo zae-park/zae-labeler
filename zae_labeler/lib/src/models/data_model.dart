d459271265a179945380d8f544a6f99740f511f6
zae-park <tom941105@gmail.com>
2024-12-12 13:16:30 +0900

    feat: add field to FileData model. - `path` is nullable field for web env

--- zae_labeler/lib/src/models/data_model.dart
+++ zae_labeler/lib/src/models/data_model.dart
@@ -8,27 +8,19 @@ class UnifiedData {
   final Map<String, dynamic>? objectData; // JSON 오브젝트 데이터
   final FileType fileType; // 파일 유형
 
-  UnifiedData(
-      {this.file, this.seriesData, this.objectData, required this.fileType});
+  UnifiedData({this.file, this.seriesData, this.objectData, required this.fileType});
 }
 
 class FileData {
   final String name; // 파일 이름
   final String type; // 파일 타입 (e.g., 'image/png', 'application/json')
+  // final FileType type;
   final String content; // 파일 내용 (base64 인코딩)
+  String? path;
 
-  FileData({required this.name, required this.type, required this.content});
+  FileData({this.path, required this.name, required this.type, required this.content});
 
   // JSON 변환 메소드
-  Map<String, dynamic> toJson() => {
-        'name': name,
-        'type': type,
-        'content': content,
-      };
-
-  static FileData fromJson(Map<String, dynamic> json) => FileData(
-        name: json['name'],
-        type: json['type'],
-        content: json['content'],
-      );
+  Map<String, dynamic> toJson() => {'name': name, 'path': path, 'type': type, 'content': content};
+  static FileData fromJson(Map<String, dynamic> json) => FileData(name: json['name'], path: json['path'] ?? "", type: json['type'], content: json['content']);
 }
