54b3466be81ace52e15d8ceecfd5222152e0b365
zae-park <tom941105@gmail.com>
2024-12-14 21:17:49 +0900

    feat: add data loader proxy

--- zae_labeler/lib/src/utils/file_loader_helper.dart
+++ zae_labeler/lib/src/utils/file_loader_helper.dart
@@ -1,1 +1,1 @@
-export 'native_file_loader.dart' if (dart.library.html) 'web_file_loader.dart';
+export './proxy_file_loader_helper/native_file_loader_helper.dart' if (dart.library.html) './proxy_file_loader_helper/web_file_loader_helper.dart';
