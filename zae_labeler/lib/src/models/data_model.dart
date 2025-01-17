c9fa2f67b728d159dab8b3c205dc75f530017a53
zae-park <tom941105@gmail.com>
2025-01-17 19:01:03 +0900

    feat: remove legacy data model

--- zae_labeler/lib/src/models/data_model.dart
+++ zae_labeler/lib/src/models/data_model.dart
@@ -1,15 +1,4 @@
-import 'dart:io';
-
 enum FileType { series, object, image, unsupported }
-
-class UnifiedData {
-  final File? file; // 파일
-  final List<double>? seriesData; // 시계열 데이터
-  final Map<String, dynamic>? objectData; // JSON 오브젝트 데이터
-  final FileType fileType; // 파일 유형
-
-  UnifiedData({this.file, this.seriesData, this.objectData, required this.fileType});
-}
 
 class FileData {
   final String name; // 파일 이름
