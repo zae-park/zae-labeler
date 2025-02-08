ad1b6ed835594bfc2ccb9021c24a47224ffb610d
zae-park <tom941105@gmail.com>
2025-02-09 03:25:36 +0900

    refactoring

--- zae_labeler/lib/src/view_models/labeling_view_model.dart
+++ zae_labeler/lib/src/view_models/labeling_view_model.dart
@@ -45,14 +45,13 @@ class LabelingViewModel extends ChangeNotifier {
       project.labelEntries = await project.loadLabelEntries();
     }
 
+    // ✅ 데이터 로딩 최적화
     if (memoryOptimized) {
-      _unifiedDataList = [];
-      if (project.dataPaths.isNotEmpty) {
-        _currentUnifiedData = await UnifiedData.fromDataPath(project.dataPaths.first);
-      }
+      _unifiedDataList.clear();
+      _currentUnifiedData = project.dataPaths.isNotEmpty ? await UnifiedData.fromDataPath(project.dataPaths.first) : UnifiedData.empty();
     } else {
       _unifiedDataList = await Future.wait(project.dataPaths.map((dpath) => UnifiedData.fromDataPath(dpath)));
-      _currentUnifiedData = _unifiedDataList.isNotEmpty ? _unifiedDataList.first : null;
+      _currentUnifiedData = _unifiedDataList.isNotEmpty ? _unifiedDataList.first : UnifiedData.empty();
     }
 
     _isInitialized = true; // ✅ 초기화 완료
@@ -106,8 +105,13 @@ class LabelingViewModel extends ChangeNotifier {
     await storageHelper.saveLabelEntry(existingEntry);
 
     // ✅ `labelEntries` 전체를 다시 로드하는 대신, 변경된 항목만 업데이트
-    final index = project.labelEntries.indexWhere((entry) => entry.dataPath == dataId);
-    project.labelEntries = List.from(project.labelEntries)..[index] = existingEntry;
+    // ✅ 기존 데이터 업데이트 (index가 없을 경우 추가)
+    int index = project.labelEntries.indexWhere((entry) => entry.dataPath == dataId);
+    if (index != -1) {
+      project.labelEntries[index] = existingEntry;
+    } else {
+      project.labelEntries.add(existingEntry);
+    }
     notifyListeners();
   }
 
@@ -127,8 +131,8 @@ class LabelingViewModel extends ChangeNotifier {
     int newIndex = _currentIndex + delta;
     if (newIndex >= 0 && newIndex < project.dataPaths.length) {
       _currentIndex = newIndex;
-      await loadCurrentData();
       notifyListeners();
+      await loadCurrentData();
     }
   }
 
