e48694591eb1414d0b2e4d5e6bdb6ddc07d69aa7
zae-park <tom941105@gmail.com>
2025-03-06 21:21:33 +0900

    feat: update configuration page

--- zae_labeler/lib/src/views/pages/configuration_page.dart
+++ zae_labeler/lib/src/views/pages/configuration_page.dart
@@ -1,23 +1,18 @@
-// This file implements the page for configuring a new project or editing an existing one.
-// Users can specify the project name, labeling mode, classes, and data directory or file uploads.
-
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:uuid/uuid.dart'; // For generating unique project IDs
-import 'package:file_picker/file_picker.dart'; // For picking directories or files
-import 'package:flutter/foundation.dart' show kIsWeb; // To determine the platform (web or native)
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import '../../models/project_model.dart';
 import '../../models/data_model.dart';
-import '../../models/label_model.dart';
 import '../../view_models/project_list_view_model.dart';
+import '../../view_models/project_view_model.dart';
 import '../widgets/labeling_mode_selector.dart';
 
 class ConfigureProjectPage extends StatefulWidget {
-  // Project to be edited, or null for creating a new one
   final Project? project;
 
   const ConfigureProjectPage({Key? key, this.project}) : super(key: key);
@@ -27,47 +22,29 @@ class ConfigureProjectPage extends StatefulWidget {
 }
 
 class ConfigureProjectPageState extends State<ConfigureProjectPage> {
-  // Form key for validating input fields
   final _formKey = GlobalKey<FormState>();
-
-  final _nameController = TextEditingController(); // project name controller
-
-  // Default labeling mode : Single Classification with 3 classes
-  LabelingMode _selectedMode = LabelingMode.singleClassification;
-  List<String> _classes = ['1', '2', '3']; // List to hold class names
-  List<DataPath> _dataPaths = [];
-
-  get path => null;
+  final _nameController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    final projectVM = Provider.of<ProjectViewModel>(context, listen: false);
 
-    // If editing an existing project, initialize fields with its data
     if (widget.project != null) {
-      _nameController.text = widget.project!.name;
-      _selectedMode = widget.project!.mode;
-      if (widget.project!.classes.isNotEmpty) {
-        _classes = widget.project!.classes;
-      }
-      if (widget.project!.dataPaths.isNotEmpty) {
-        _dataPaths = widget.project!.dataPaths;
-      }
+      _nameController.text = projectVM.project.name;
     }
   }
 
   @override
   void dispose() {
-    _nameController.dispose(); // Dispose controller to free resources
+    _nameController.dispose();
     super.dispose();
   }
 
-  // Add a new class to the project
   void _addClass() {
     showDialog(
       context: context,
       builder: (context) {
-        // Controller for class name input
         final classController = TextEditingController();
 
         return AlertDialog(
@@ -77,9 +54,8 @@ class ConfigureProjectPageState extends State<ConfigureProjectPage> {
             TextButton(
               onPressed: () {
                 if (classController.text.isNotEmpty) {
-                  // Add new class to the list
-                  setState(() => _classes.add(classController.text));
-                  Navigator.pop(context); // Close the dialog
+                  setState(() => Provider.of<ProjectViewModel>(context, listen: false).addClass(classController.text));
+                  Navigator.pop(context);
                 }
               },
               child: const Text('Add'),
@@ -90,77 +66,56 @@ class ConfigureProjectPageState extends State<ConfigureProjectPage> {
     );
   }
 
-  // Remove a class from the project
-  void _removeClass(int index) => setState(() => _classes.removeAt(index));
+  void _removeClass(int index) {
+    setState(() => Provider.of<ProjectViewModel>(context, listen: false).removeClass(index));
+  }
 
-  // Pick a data directory or files depending on the platform
   Future<void> _pickData() async {
+    final projectVM = Provider.of<ProjectViewModel>(context, listen: false);
+
     if (kIsWeb) {
-      // Web: Use file picker for file uploads
-      FilePickerResult? result = await FilePicker.platform.pickFiles(
-        allowMultiple: true,
-        withData: true, // Ensure file data is loaded
-      );
+      FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true, withData: true);
 
       if (result != null) {
         setState(() {
           for (var file in result.files) {
-            _dataPaths.add(DataPath(fileName: file.name, base64Content: base64Encode(file.bytes ?? [])));
+            projectVM.addDataPath(DataPath(fileName: file.name, base64Content: base64Encode(file.bytes ?? [])));
           }
         });
       }
     } else {
-      // Native: Use directory picker
       String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
       if (selectedDirectory != null) {
         setState(() {
           final directory = Directory(selectedDirectory);
           final files = directory.listSync().whereType<File>();
           for (var file in files) {
-            _dataPaths.add(DataPath(fileName: path.basename(file.path), filePath: file.path));
+            projectVM.addDataPath(DataPath(fileName: file.uri.pathSegments.last, filePath: file.path));
           }
         });
       }
     }
   }
 
-  void _confirmProject() async {
+  Future<void> _confirmProject() async {
     if (_formKey.currentState!.validate()) {
-      final projectVM = Provider.of<ProjectListViewModel>(context, listen: false);
-      final isNewProject = widget.project == null;
+      final projectVM = Provider.of<ProjectViewModel>(context, listen: false);
+      final projectListVM = Provider.of<ProjectListViewModel>(context, listen: false);
 
-      final updatedProject = Project(
-        id: widget.project?.id ?? const Uuid().v4(),
-        name: _nameController.text,
-        mode: _selectedMode,
-        classes: _classes,
-        dataPaths: _dataPaths,
+      bool isNewProject = widget.project == null;
+      if (!isNewProject && projectVM.isLabelingModeChanged()) {
+        bool confirmChange = await _showLabelingModeChangeDialog(context);
+        if (!confirmChange) return;
+      }
+
+      await projectVM.saveProject(isNewProject);
+      projectListVM.loadProjects(); // 프로젝트 목록 업데이트
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('${projectVM.project.name} project has been ${isNewProject ? 'created' : 'updated'}.')),
       );
 
-      if (!isNewProject && widget.project!.mode != _selectedMode) {
-        bool confirmChange = await _showLabelingModeChangeDialog(context);
-        if (!confirmChange || !mounted) return;
-      }
-
-      if (isNewProject) {
-        projectVM.saveProject(updatedProject);
-        if (!mounted) return;
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('${updatedProject.name} project has been created.')),
-        );
-      } else {
-        debugPrint("📝 프로젝트 업데이트 시도: ${updatedProject.name}");
-        await projectVM.updateProject(context, updatedProject);
-        debugPrint("✅ 프로젝트 업데이트 완료");
-
-        if (!mounted) return;
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('${updatedProject.name} project has been updated.')),
-        );
-      }
-
       if (!mounted) return;
-      debugPrint("🔙 Navigator.pop 실행!");
       Navigator.pop(context);
     }
   }
@@ -184,71 +139,51 @@ class ConfigureProjectPageState extends State<ConfigureProjectPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.project == null ? 'Create Project' : 'Edit Project')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              // Project name input
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Project Name'),
-                validator: (value) => (value == null || value.isEmpty) ? "Please enter a project name" : null,
-              ),
-              const SizedBox(height: 16),
-              // Labeling mode dropdown
-              LabelingModeSelector.dropdown(selectedMode: _selectedMode, onModeChanged: (newMode) => setState(() => _selectedMode = newMode)),
-              const SizedBox(height: 16),
-              // Class list section
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Consumer<ProjectViewModel>(
+      builder: (context, projectVM, child) {
+        return Scaffold(
+          appBar: AppBar(title: Text(widget.project == null ? 'Create Project' : 'Edit Project')),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: ListView(
                 children: [
-                  const Text('Classes', style: TextStyle(fontSize: 16)),
-                  IconButton(icon: const Icon(Icons.add), onPressed: _addClass, tooltip: 'Add Class'),
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(labelText: 'Project Name'),
+                    onChanged: (value) => projectVM.setName(value),
+                    validator: (value) => (value == null || value.isEmpty) ? "Please enter a project name" : null,
+                  ),
+                  const SizedBox(height: 16),
+                  LabelingModeSelector.dropdown(
+                    selectedMode: projectVM.project.mode,
+                    onModeChanged: (newMode) => projectVM.setLabelingMode(newMode),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Classes', style: TextStyle(fontSize: 16)),
+                      IconButton(icon: const Icon(Icons.add), onPressed: _addClass, tooltip: 'Add Class'),
+                    ],
+                  ),
+                  ...projectVM.project.classes.asMap().entries.map((entry) {
+                    int index = entry.key;
+                    String className = entry.value;
+                    return ListTile(
+                      title: Text(className),
+                      trailing: IconButton(icon: const Icon(Icons.delete, color: Colors.red), onPressed: () => _removeClass(index), tooltip: 'Remove Class'),
+                    );
+                  }).toList(),
+                  const SizedBox(height: 16),
+                  ElevatedButton(onPressed: _confirmProject, child: Text(widget.project == null ? 'Create Project' : 'Save Changes')),
                 ],
               ),
-              // Display list of classes
-              ..._classes.asMap().entries.map((entry) {
-                int index = entry.key;
-                String className = entry.value;
-                return ListTile(
-                  title: Text(className),
-                  trailing: IconButton(icon: const Icon(Icons.delete, color: Colors.red), onPressed: () => _removeClass(index), tooltip: 'Remove Class'),
-                );
-              }).toList(),
-              const SizedBox(height: 16),
-              // Data directory or file selection
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      readOnly: true, // Make the field read-only
-                      decoration: const InputDecoration(
-                        labelText: kIsWeb ? 'Uploaded File Names' : 'Data Directory Path',
-                        hintText: kIsWeb ? 'Upload files' : 'Select a directory',
-                      ),
-                      controller: TextEditingController(
-                        text: _dataPaths.map((dp) => dp.fileName).join(', '),
-                      ),
-                      validator: (value) => (value == null || value.isEmpty) ? (kIsWeb ? 'Please upload files' : 'Please select a directory') : null,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(kIsWeb ? Icons.upload_file : Icons.folder_open),
-                    onPressed: _pickData, // Trigger directory or file picker
-                    tooltip: kIsWeb ? 'Upload Files' : 'Select Directory',
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(onPressed: _confirmProject, child: Text(widget.project == null ? 'Create Project' : 'Save Changes')),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
