810c02a6e0b54bad7a8f32a9d3924ea4db08c255
zae-park <tom941105@gmail.com>
2025-03-06 19:36:29 +0900

    feat: update mockClass

--- zae_labeler/test/mocks/mock_storage_helper.dart
+++ zae_labeler/test/mocks/mock_storage_helper.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'package:zae_labeler/src/utils/proxy_storage_helper/interface_storage_helper.dart';
 import 'package:zae_labeler/src/models/project_model.dart';
-import 'package:zae_labeler/src/models/label_entry.dart';
+import 'package:zae_labeler/src/models/label_model.dart';
 import 'package:zae_labeler/src/models/data_model.dart';
 
 class MockStorageHelper implements StorageHelperInterface {
   final Map<String, String> _mockDatabase = {}; // ✅ 인메모리 데이터 저장
 
   @override
-  Future<List<Project>> loadProjects() async {
+  Future<List<Project>> loadProjectFromConfig(String projectConfig) async {
     if (_mockDatabase.containsKey('projects')) {
       final jsonData = jsonDecode(_mockDatabase['projects']!);
       return (jsonData as List).map((e) => Project.fromJson(e)).toList();
@@ -17,57 +17,59 @@ class MockStorageHelper implements StorageHelperInterface {
   }
 
   @override
-  Future<void> saveProjects(List<Project> projects) async {
+  Future<void> saveProjectConfig(List<Project> projects) async {
     _mockDatabase['projects'] = jsonEncode(projects.map((e) => e.toJson()).toList());
   }
 
   @override
-  Future<List<LabelEntry>> loadLabelEntries(String projectId) async {
-    if (_mockDatabase.containsKey('labels')) {
-      final jsonData = jsonDecode(_mockDatabase['labels']!);
-      return (jsonData as List).map((e) => LabelEntry.fromJson(e)).toList();
+  Future<void> saveLabelData(String projectId, String dataPath, LabelModel labelModel) async {
+    final key = 'label_${projectId}_$dataPath';
+    _mockDatabase[key] = jsonEncode({
+      'mode': labelModel.runtimeType.toString(),
+      'labeled_at': labelModel.labeledAt.toIso8601String(),
+      'label_data': labelModel.labelData,
+    });
+  }
+
+  @override
+  Future<LabelModel> loadLabelData(String projectId, String dataPath, LabelingMode mode) async {
+    final key = 'label_${projectId}_$dataPath';
+    if (_mockDatabase.containsKey(key)) {
+      final jsonData = jsonDecode(_mockDatabase[key]!);
+      return LabelModelConverter.fromJson(mode, jsonData['label_data']);
     }
+    return LabelModelConverter.fromJson(mode, {});
+  }
+
+  @override
+  Future<String> exportAllLabels(Project project, List<LabelModel> labelModels, List<DataPath> fileDataList) async {
+    return 'mock_export_path.zip';
+  }
+
+  @override
+  Future<List<LabelModel>> importAllLabels() async {
     return [];
   }
 
   @override
-  Future<void> saveLabelEntries(String projectId, List<LabelEntry> labelEntries) async {
-    _mockDatabase['labels'] = jsonEncode(labelEntries.map((e) => e.toJson()).toList());
+  Future<List<LabelModel>> loadAllLabels(String projectId) async {
+    final labels = _mockDatabase.keys.where((key) => key.startsWith('label_$projectId')).toList();
+    return labels.map((key) {
+      final jsonData = jsonDecode(_mockDatabase[key]!);
+      final mode = LabelingMode.values.firstWhere((e) => e.toString() == jsonData['mode']);
+      return LabelModelConverter.fromJson(mode, jsonData['label_data']);
+    }).toList();
   }
 
   @override
-  Future<void> saveLabelEntry(String projectId, LabelEntry labelEntry) async {
-    List<LabelEntry> existingEntries = await loadLabelEntries(projectId);
-    int index = existingEntries.indexWhere((entry) => entry.dataFilename == labelEntry.dataFilename);
-    if (index != -1) {
-      existingEntries[index] = labelEntry;
-    } else {
-      existingEntries.add(labelEntry);
-    }
-    _mockDatabase['labels'] = jsonEncode(existingEntries.map((e) => e.toJson()).toList());
+  Future<String> downloadProjectConfig(Project project) {
+    // TODO: implement downloadProjectConfig
+    throw UnimplementedError();
   }
 
   @override
-  Future<LabelEntry> loadLabelEntry(String projectId, String dataPath) async {
-    List<LabelEntry> existingEntries = await loadLabelEntries(projectId);
-    return existingEntries.firstWhere(
-      (entry) => entry.dataPath == dataPath,
-      orElse: () => LabelEntry.empty(), // ✅ null 대신 기본값 반환
-    );
-  }
-
-  @override
-  Future<String> downloadLabelsAsZip(Project project, List<LabelEntry> labelEntries, List<DataPath> fileDataList) async {
-    return 'mock_zip_path.zip';
-  }
-
-  @override
-  Future<String> downloadProjectConfig(Project project) async {
-    return 'mock_project_config.json';
-  }
-
-  @override
-  Future<List<LabelEntry>> importLabelEntries() async {
-    return [];
+  Future<void> saveAllLabels(String projectId, List<LabelModel> labels) {
+    // TODO: implement saveAllLabels
+    throw UnimplementedError();
   }
 }
