a8f4f0e7e01d160793b6a8a2084e8a3f0066fb91
zae-park <tom941105@gmail.com>
2025-03-23 17:16:02 +0900

    test: add test case for using MockStorageHelper

--- zae_labeler/test/utils/storage_helper_test.dart
+++ zae_labeler/test/utils/storage_helper_test.dart
@@ -1,59 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:zae_labeler/src/models/data_model.dart';
 import 'package:zae_labeler/src/models/label_model.dart';
 import 'package:zae_labeler/src/models/project_model.dart';
-import 'package:zae_labeler/src/models/sub_models/classification_label_model.dart';
-import 'package:zae_labeler/src/utils/storage_helper.dart';
-import 'package:zae_labeler/src/utils/proxy_storage_helper/interface_storage_helper.dart';
 
-class MockStorageHelper implements StorageHelperInterface {
-  List<Project> savedProjects = [];
-
-  @override
-  Future<void> saveProjectConfig(List<Project> projects) async {
-    savedProjects = projects;
-  }
-
-  @override
-  Future<List<Project>> loadProjectFromConfig(String config) async {
-    return savedProjects;
-  }
-
-  @override
-  Future<String> downloadProjectConfig(Project project) async {
-    return '/mock/path/${project.name}_config.json';
-  }
-
-  @override
-  Future<void> saveProjectList(List<Project> projects) async {}
-
-  @override
-  Future<List<Project>> loadProjectList() async => [];
-
-  @override
-  Future<void> saveLabelData(String projectId, String dataPath, LabelModel labelModel) async {}
-
-  @override
-  Future<LabelModel> loadLabelData(String projectId, String dataPath, LabelingMode mode) async => SingleClassificationLabelModel.empty();
-
-  @override
-  Future<void> saveAllLabels(String projectId, List<LabelModel> labels) async {}
-
-  @override
-  Future<List<LabelModel>> loadAllLabels(String projectId) async => [];
-
-  @override
-  Future<void> deleteProjectLabels(String projectId) async {}
-
-  @override
-  Future<String> exportAllLabels(Project project, List<LabelModel> labelModels, List<DataPath> fileDataList) async => '/mock/path/${project.name}_labels.zip';
-
-  @override
-  Future<List<LabelModel>> importAllLabels() async => [];
-
-  @override
-  Future<void> clearAllCache() async {}
-}
+import '../mocks/mock_storage_helper.dart';
 
 void main() {
   late MockStorageHelper mockHelper;
