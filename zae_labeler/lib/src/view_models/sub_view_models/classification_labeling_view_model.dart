69044cb47558a4020c2cfe800f4911929ee7cbda
zae-park <tom941105@gmail.com>
2025-03-30 22:51:05 +0900

    feat: add initial classificationLabelingVM

---
+++ zae_labeler/lib/src/view_models/sub_view_models/classification_labeling_view_model.dart
@@ -0,0 +1,31 @@
+import '../../models/sub_models/classification_label_model.dart';
+import 'base_label_view_model.dart';
+
+class ClassificationLabelingViewModel extends LabelingViewModel {
+  ClassificationLabelingViewModel({
+    required super.project,
+    required super.storageHelper,
+  });
+
+  @override
+  Future<void> toggleLabel(String labelItem) async {
+    final labelVM = currentLabelVM;
+    if (labelVM.labelModel is ClassificationLabelModel) {
+      labelVM.labelModel = (labelVM.labelModel as ClassificationLabelModel).toggleLabel(labelItem);
+      await labelVM.saveLabel();
+      notifyListeners();
+    }
+  }
+
+  @override
+  Future<void> updateLabel(dynamic labelData) async {
+    currentLabelVM.updateLabel(labelData);
+    await currentLabelVM.saveLabel();
+    notifyListeners();
+  }
+
+  @override
+  bool isLabelSelected(String labelItem) {
+    return currentLabelVM.labelModel.isSelected(labelItem);
+  }
+}
