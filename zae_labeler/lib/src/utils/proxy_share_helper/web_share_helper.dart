39cb726c1c7fccf544a2c166f7bb0fbd0e0d1c65
zae-park <tom941105@gmail.com>
2025-04-05 22:22:47 +0900

    feat: minor update

--- zae_labeler/lib/src/utils/proxy_share_helper/web_share_helper.dart
+++ zae_labeler/lib/src/utils/proxy_share_helper/web_share_helper.dart
@@ -2,10 +2,7 @@ import 'dart:html' as html;
 
 /// ✅ 웹에서 텍스트 공유
 Future<void> shareTextOnWeb(String title, String text) async {
-  await html.window.navigator.share({
-    'title': title,
-    'text': text,
-  });
+  await html.window.navigator.share({'title': title, 'text': text});
 }
 
 /// ❌ 웹에서는 파일 공유를 지원하지 않음
