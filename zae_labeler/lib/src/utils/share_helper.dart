2a5e372332b2a8c015ff1e22143960cbb9e88700
zae-park <tom941105@gmail.com>
2025-04-06 01:29:13 +0900

    feat: update proxy

--- zae_labeler/lib/src/utils/share_helper.dart
+++ zae_labeler/lib/src/utils/share_helper.dart
@@ -1,10 +1,6 @@
-import './proxy_share_helper/stub_share_helper.dart'
-    if (dart.library.html) './proxy_share_helper/web_share_helper.dart'
-    if (dart.library.io) './proxy_share_helper/native_share_helper.dart';
-
-export './proxy_share_helper/stub_share_helper.dart'
-    if (dart.library.html) './proxy_share_helper/web_share_helper.dart'
-    if (dart.library.io) './proxy_share_helper/native_share_helper.dart';
+import './proxy_share_helper/stub_share_helper.dart';
+import './proxy_share_helper/native_share_helper.dart' if (dart.library.html) './proxy_share_helper/web_share_helper.dart';
+export './proxy_share_helper/stub_share_helper.dart';
 
 class ShareHelper extends ShareHelperInterface {
   static final _instance = ShareHelperInterface();
