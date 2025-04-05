7ad3d89810e8c885ded61d6a05da2d47ae4bb2d9
zae-park <tom941105@gmail.com>
2025-04-06 02:18:13 +0900

    feat: use Singleton ShareHelper

--- zae_labeler/lib/src/views/pages/project_list_page.dart
+++ zae_labeler/lib/src/views/pages/project_list_page.dart
@@ -3,7 +3,7 @@ import 'dart:io' as io;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:file_picker/file_picker.dart';
-import '../../utils/share_helper.dart';
+import 'package:zae_labeler/src/utils/share_helper.dart';
 import '../../view_models/project_list_view_model.dart';
 import '../../view_models/project_view_model.dart';
 import '../../view_models/locale_view_model.dart';
@@ -116,7 +116,7 @@ class _ProjectListPageState extends State<ProjectListPage> {
                     final project = projectListVM.projects[index];
 
                     return ChangeNotifierProvider(
-                      create: (context) => ProjectViewModel(storageHelper: StorageHelper.instance, project: project, shareHelper: ShareHelper.instance),
+                      create: (context) => ProjectViewModel(storageHelper: StorageHelper.instance, project: project, shareHelper: getShareHelper()),
                       child: Consumer<ProjectViewModel>(
                         builder: (context, projectVM, _) {
                           debugPrint("[LabelingPage 진입] project.mode = ${project.mode}");
