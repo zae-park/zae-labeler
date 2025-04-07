9a687c58d23457736c6bcd35acaa4635bd282c43
zae-park <tom941105@gmail.com>
2025-04-07 23:40:55 +0900

    feat: add stub implementation

---
+++ zae_labeler/lib/src/utils/proxy_storage_helper/stub_storage_helper.dart
@@ -0,0 +1,7 @@
+// lib/src/utils/proxy_storage_helper/storage_stub.dart
+import 'interface_storage_helper.dart';
+
+class StorageHelperImpl implements StorageHelperInterface {
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
