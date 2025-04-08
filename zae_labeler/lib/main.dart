0a08a346dce3723e0a6cacd9fe70ef24a5c71e2b
zae-park <tom941105@gmail.com>
2025-04-09 00:31:53 +0900

    feat: apply cloudStorageHelper for Firebase

--- zae_labeler/lib/main.dart
+++ zae_labeler/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'src/utils/proxy_storage_helper/cloud_storage_helper.dart';
 import 'src/utils/storage_helper.dart';
 import 'src/view_models/auth_view_model.dart';
 import 'src/views/pages/auth_gate.dart';
@@ -27,7 +29,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       // Registering providers for state management
       providers: [
-        ChangeNotifierProvider<ProjectListViewModel>(create: (_) => ProjectListViewModel(storageHelper: StorageHelper.instance)),
+        // ChangeNotifierProvider<ProjectListViewModel>(create: (_) => ProjectListViewModel(storageHelper: StorageHelper.instance)),
+        ChangeNotifierProvider<ProjectListViewModel>(
+            create: (_) => ProjectListViewModel(storageHelper: kIsWeb ? CloudStorageHelper() : StorageHelper.instance)),
         ChangeNotifierProvider<LocaleViewModel>(create: (_) => LocaleViewModel()),
         Provider<StorageHelperInterface>.value(value: StorageHelper.instance),
         ChangeNotifierProvider<AuthViewModel>(create: (_) => AuthViewModel()),
