f2f7d79de7c96547094c5662fa226b043252324f
zae-park <tom941105@gmail.com>
2025-04-11 01:02:23 +0900

    feat: add github login to loginPage

--- zae_labeler/lib/src/views/pages/auth_gate.dart
+++ zae_labeler/lib/src/views/pages/auth_gate.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../../view_models/auth_view_model.dart';
 import 'project_list_page.dart';
 import '../../../env.dart';
+import 'package:zae_labeler/common/common_widgets.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -21,16 +22,29 @@ class AuthGate extends StatelessWidget {
           );
         }
 
-        return Scaffold(
-          body: Center(
-            child: ElevatedButton.icon(
-              icon: const Icon(Icons.login),
-              label: const Text("Sign in with Google"),
-              onPressed: () => authVM.signInWithGoogle(),
-            ),
-          ),
-        );
+        return const LoginScreen();
       },
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authVM = context.read<AuthViewModel>();
+
+    return Scaffold(
+      body: Center(
+        child: AutoSeparatedColumn(
+          separator: const SizedBox(height: 12),
+          children: [
+            ElevatedButton.icon(icon: const Icon(Icons.login), label: const Text("Sign in with Google"), onPressed: authVM.signInWithGoogle),
+            ElevatedButton.icon(icon: const Icon(Icons.code), label: const Text("Sign in with GitHub"), onPressed: authVM.signInWithGitHub),
+          ],
+        ),
+      ),
     );
   }
 }
