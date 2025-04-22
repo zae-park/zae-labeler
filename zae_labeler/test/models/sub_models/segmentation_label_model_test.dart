447d765c346a3512ff1bfa746abaf67e68dd86d6
zae-park <tom941105@gmail.com>
2025-04-23 00:42:43 +0900

    test: update test case

--- zae_labeler/test/models/sub_models/segmentation_label_model_test.dart
+++ zae_labeler/test/models/sub_models/segmentation_label_model_test.dart
@@ -3,21 +3,30 @@ import 'package:zae_labeler/src/models/sub_models/segmentation_label_model.dart'
 
 void main() {
   group('SegmentationLabelModel', () {
-    test('SingleClassSegmentationLabelModel pixel add/remove', () {
-      final model =
-          SingleClassSegmentationLabelModel.empty().copyWith(label: SegmentationData(segments: {'object': Segment(indices: {}, classLabel: 'object')}));
+    test('SingleClassSegmentationLabelModel pixel add/remove and status', () {
+      final model = SingleClassSegmentationLabelModel.empty();
+
+      expect(model.isLabeled, isFalse);
 
       final added = model.addPixel(1, 1, "sample");
-      final removed = added.removePixel(1, 1);
+      expect(added.label?.segments['sample']!.containsPixel(1, 1), isTrue);
+      expect(added.isLabeled, isTrue);
 
-      expect(added.label!.segments['sample']!.containsPixel(1, 1), isTrue);
-      expect(removed.label!.segments['sample'], isNull);
+      final removed = added.removePixel(1, 1);
+      expect(removed.label?.segments['sample'], isNull);
+      expect(removed.isLabeled, isFalse);
     });
 
-    test('MultiClassSegmentationLabelModel pixel add/remove', () {
-      final model = MultiClassSegmentationLabelModel.empty().addPixel(2, 2, 'person').removePixel(2, 2);
+    test('MultiClassSegmentationLabelModel pixel add/remove and status', () {
+      final model = MultiClassSegmentationLabelModel.empty();
 
-      expect(model.label!.segments['person'], isNull);
+      final added = model.addPixel(2, 2, 'person');
+      expect(added.label?.segments['person']?.containsPixel(2, 2), isTrue);
+      expect(added.isLabeled, isTrue);
+
+      final removed = added.removePixel(2, 2);
+      expect(removed.label?.segments['person'], isNull);
+      expect(removed.isLabeled, isFalse);
     });
   });
 }
