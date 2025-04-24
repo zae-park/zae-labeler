a1822644c70704aa9925849494bc80e5a4348b6d
zae-park <tom941105@gmail.com>
2025-04-24 23:28:54 +0900

    feat: optimize main.dart

--- zae_labeler/lib/main.dart
+++ zae_labeler/lib/main.dart
@@ -1,26 +1,23 @@
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'src/utils/proxy_storage_helper/cloud_storage_helper.dart';
-import 'src/utils/storage_helper.dart';
-import 'src/view_models/auth_view_model.dart';
-import 'src/views/pages/auth_gate.dart';
-// import 'src/views/pages/project_list_page.dart';
-import 'src/views/pages/configuration_page.dart';
-import 'src/views/pages/labeling_page.dart';
-import 'src/view_models/project_list_view_model.dart';
-import 'src/view_models/locale_view_model.dart';
-import 'env.dart';
 
 import 'firebase_options.dart';
+import 'env.dart';
+import 'src/utils/storage_helper.dart';
+import 'src/utils/proxy_storage_helper/cloud_storage_helper.dart';
+import 'src/view_models/auth_view_model.dart';
+import 'src/view_models/project_list_view_model.dart';
+import 'src/view_models/locale_view_model.dart';
+import 'src/views/pages/auth_gate.dart';
+import 'src/views/pages/configuration_page.dart';
+import 'src/views/pages/labeling_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await FirebaseAuth.instance.authStateChanges().firstWhere((u) => u != null);
 
   runApp(const MyApp());
 }
@@ -31,27 +28,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      // Registering providers for state management
       providers: [
-        // ChangeNotifierProvider<ProjectListViewModel>(create: (_) => ProjectListViewModel(storageHelper: StorageHelper.instance)),
         ChangeNotifierProvider<ProjectListViewModel>(
             create: (_) => ProjectListViewModel(storageHelper: kIsWeb ? CloudStorageHelper() : StorageHelper.instance)),
         ChangeNotifierProvider<LocaleViewModel>(create: (_) => LocaleViewModel()),
-        Provider<StorageHelperInterface>.value(value: isProd ? CloudStorageHelper() : StorageHelper.instance),
         ChangeNotifierProvider<AuthViewModel>(create: (_) => AuthViewModel()),
+        Provider<StorageHelperInterface>.value(value: isProd ? CloudStorageHelper() : StorageHelper.instance),
       ],
       child: Consumer<LocaleViewModel>(
         builder: (context, localeVM, child) {
           return MaterialApp(
-            // Application title displayed in task switcher or web browser tab
             title: 'Data Labeling App for YOU!',
             theme: ThemeData(primarySwatch: Colors.blue),
-
-            // Set the current locale dynamically
             locale: localeVM.currentLocale,
             supportedLocales: const [Locale('en'), Locale('ko')],
-
-            // Enable localization delegates
             localizationsDelegates: const [
               GlobalMaterialLocalizations.delegate,
               GlobalWidgetsLocalizations.delegate,
