d71acf63483f88dc4684f412c3d46df719c408d1
zae-park <tom941105@gmail.com>
2025-05-13 00:53:17 +0900

    feat: replace Notifier from ProjectTile builder to projectTile

--- zae_labeler/lib/src/views/widgets/project_tile.dart
+++ zae_labeler/lib/src/views/widgets/project_tile.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../../models/project_model.dart';
 import '../../utils/share_helper.dart';
+import '../../view_models/project_list_view_model.dart';
 import '../../view_models/project_view_model.dart';
+import '../../view_models/configuration_view_model.dart';
+import '../../views/pages/configuration_page.dart';
 
 class ProjectTile extends StatelessWidget {
   final Project project;
-  final VoidCallback onEdit;
-  final VoidCallback onDownload;
-  final VoidCallback onShare;
-  final VoidCallback onDelete;
-  final VoidCallback onTap;
 
-  const ProjectTile({
-    Key? key,
-    required this.project,
-    required this.onEdit,
-    required this.onDownload,
-    required this.onShare,
-    required this.onDelete,
-    required this.onTap,
-  }) : super(key: key);
+  const ProjectTile({Key? key, required this.project}) : super(key: key);
+
+  void _openLabelingPage(BuildContext context, Project p) {
+    Navigator.pushNamed(context, '/labeling', arguments: p);
+  }
+
+  void _openEditPage(BuildContext context, Project p) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => ChangeNotifierProvider(
+          create: (_) => ConfigurationViewModel.fromProject(p),
+          child: const ConfigureProjectPage(),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _confirmDelete(BuildContext context, Project project, ProjectViewModel vm) async {
+    bool? confirmed = await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Delete Project'),
+        content: Text('Are you sure you want to delete the project "${project.name}"?'),
+        actions: [
+          TextButton(onPressed: () => Navigator.pop(context, false), child: const Text('Cancel')),
+          TextButton(onPressed: () => Navigator.pop(context, true), child: const Text('Delete', style: TextStyle(color: Colors.red))),
+        ],
+      ),
+    );
+
+    if (confirmed == true) {
+      final projectListVM = Provider.of<ProjectListViewModel>(context, listen: false);
+      await vm.deleteProject();
+      await projectListVM.removeProject(project.id);
+
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Deleted project: ${project.name}')));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,13 +59,18 @@ class ProjectTile extends StatelessWidget {
       create: (_) => ProjectViewModel(storageHelper: Provider.of(context, listen: false), shareHelper: getShareHelper(), project: project),
       child: Consumer<ProjectViewModel>(
         builder: (context, vm, _) {
-          final actionMap = <String, VoidCallback>{'edit': onEdit, 'download': onDownload, 'share': onShare, 'delete': onDelete};
+          final actionMap = <String, VoidCallback>{
+            'edit': () => _openEditPage(context, vm.project),
+            'download': () => vm.downloadProjectConfig(),
+            'share': () => vm.shareProject(context),
+            'delete': () => _confirmDelete(context, vm.project, vm),
+          };
 
           return Card(
             child: ListTile(
               title: Text(project.name),
               subtitle: Text(project.mode.displayName),
-              onTap: onTap,
+              onTap: () => _openLabelingPage(context, vm.project),
               trailing: PopupMenuButton<String>(
                 onSelected: (value) => actionMap[value]?.call(),
                 itemBuilder: (context) => [
