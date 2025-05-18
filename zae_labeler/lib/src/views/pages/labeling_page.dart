c51aa1f5c53226ab303302cbed6724091fb8e132
zae-park <tom941105@gmail.com>
2025-05-19 00:20:28 +0900

    feat: update LabelingPage

--- zae_labeler/lib/src/views/pages/labeling_page.dart
+++ zae_labeler/lib/src/views/pages/labeling_page.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../models/label_model.dart';
 import '../../models/project_model.dart';
+import '../../view_models/labeling_view_model.dart';
 import 'not_found_page.dart';
 import 'sub_pages/classification_labeling_page.dart';
 import 'sub_pages/segmentation_labeling_page.dart';
 import 'sub_pages/cross_classification_labeling_page.dart';
 
-class LabelingPage extends StatefulWidget {
+class LabelingPage extends StatelessWidget {
   final Project project;
+  final LabelingViewModel viewModel;
 
-  const LabelingPage({Key? key, required this.project}) : super(key: key);
-
-  @override
-  LabelingPageState createState() => LabelingPageState();
-}
-
-class LabelingPageState extends State<LabelingPage> {
-  final modeToPageBuilder = {
-    LabelingMode.singleClassification: (p) => ClassificationLabelingPage(project: p),
-    LabelingMode.multiClassification: (p) => ClassificationLabelingPage(project: p),
-    LabelingMode.crossClassification: (p) => CrossClassificationLabelingPage(project: p),
-    LabelingMode.singleClassSegmentation: (p) => SegmentationLabelingPage(project: p),
-    LabelingMode.multiClassSegmentation: (p) => SegmentationLabelingPage(project: p),
-  };
+  const LabelingPage({Key? key, required this.project, required this.viewModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return modeToPageBuilder[widget.project.mode]?.call(widget.project) ?? const NotFoundPage();
+    final modeToPageBuilder = {
+      LabelingMode.singleClassification: (Project p, LabelingViewModel vm) => ClassificationLabelingPage(project: p, viewModel: vm),
+      LabelingMode.multiClassification: (Project p, LabelingViewModel vm) => ClassificationLabelingPage(project: p, viewModel: vm),
+      LabelingMode.crossClassification: (Project p, LabelingViewModel vm) => CrossClassificationLabelingPage(project: p, viewModel: vm),
+      LabelingMode.singleClassSegmentation: (Project p, LabelingViewModel vm) => SegmentationLabelingPage(project: p, viewModel: vm),
+      LabelingMode.multiClassSegmentation: (Project p, LabelingViewModel vm) => SegmentationLabelingPage(project: p, viewModel: vm),
+    };
+
+    final builder = modeToPageBuilder[project.mode];
+    if (builder == null) return const NotFoundPage();
+
+    return ChangeNotifierProvider<LabelingViewModel>.value(value: viewModel, child: builder(project, viewModel));
   }
 }
