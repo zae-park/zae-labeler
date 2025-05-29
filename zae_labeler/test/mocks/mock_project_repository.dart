d02509e981115d2e10a5477d77463c1f7e0c17a9
zae-park <tom941105@gmail.com>
2025-05-29 23:00:51 +0900

    test: add mock

---
+++ zae_labeler/test/mocks/mock_project_repository.dart
@@ -0,0 +1,4 @@
+import 'package:mockito/mockito.dart';
+import 'package:zae_labeler/src/repositories/project_repository.dart';
+
+class MockProjectRepository extends Mock implements ProjectRepository {}
