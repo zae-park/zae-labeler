822768fe3bf3b7c1546b1b9283a6a58bf19d3b5f
zae-park <tom941105@gmail.com>
2025-05-29 23:27:57 +0900

    feat: update saveProjectUseCase

--- zae_labeler/lib/src/domain/project/save_project_use_case.dart
+++ zae_labeler/lib/src/domain/project/save_project_use_case.dart
@@ -1,30 +1,24 @@
 // lib/src/domain/project/save_project_use_case.dart
 
 import '../../models/project_model.dart';
-import '../../utils/storage_helper.dart';
+import '../../repositories/project_repository.dart';
 import '../validator/project_validator.dart';
 
 /// ✅ UseCase: 프로젝트 저장 (단일 or 전체)
 class SaveProjectUseCase {
-  final StorageHelperInterface storageHelper;
+  final ProjectRepository repository;
 
-  SaveProjectUseCase({required this.storageHelper});
+  SaveProjectUseCase({required this.repository});
 
-  /// 🔹 단일 프로젝트 저장 (내부적으로 전체 리스트 불러오기)
+  /// 🔹 단일 프로젝트 저장
+  /// - 유효성 검사 후 repository를 통해 저장
   Future<void> saveOne(Project project) async {
     ProjectValidator.validate(project);
-    final currentList = await storageHelper.loadProjectList();
-    final index = currentList.indexWhere((p) => p.id == project.id);
-    if (index != -1) {
-      currentList[index] = project;
-    } else {
-      currentList.add(project);
-    }
-
-    await saveAll(currentList);
+    await repository.saveProject(project);
   }
 
+  /// 🔹 전체 프로젝트 리스트 저장
   Future<void> saveAll(List<Project> projects) async {
-    await storageHelper.saveProjectList(projects);
+    await repository.saveAll(projects);
   }
 }
