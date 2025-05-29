999ba2cb4deaba75a571d2430f6d501cf27e244d
zae-park <tom941105@gmail.com>
2025-05-30 00:39:12 +0900

    test: enhance test for ProjectVM using repo

--- zae_labeler/test/src/view_models/project_view_model_test.dart
+++ zae_labeler/test/src/view_models/project_view_model_test.dart
@@ -1,28 +1,25 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:zae_labeler/src/models/data_model.dart';
 import 'package:zae_labeler/src/view_models/project_view_model.dart';
 import 'package:zae_labeler/src/models/label_model.dart';
-import '../../mocks/mock_storage_helper.dart';
+import 'package:zae_labeler/src/models/data_model.dart';
+import '../../mocks/mock_project_repository.dart';
 import '../../mocks/mock_share_helper.dart';
 
 void main() {
-  group('ProjectViewModel', () {
-    if (!kIsWeb) {
-      test('skipped on non-web', () => expect(true, isTrue), skip: true);
-      return;
-    }
+  group('ProjectViewModel (Refactored)', () {
     late ProjectViewModel viewModel;
-    late MockStorageHelper mockHelper;
+    late MockProjectRepository mockRepository;
+    late MockShareHelper mockShare;
 
     setUp(() {
-      mockHelper = MockStorageHelper();
-      viewModel = ProjectViewModel(storageHelper: mockHelper, shareHelper: MockShareHelper());
+      mockRepository = MockProjectRepository();
+      mockShare = MockShareHelper();
+      viewModel = ProjectViewModel(repository: mockRepository, shareHelper: mockShare);
     });
 
     test('setName updates project name', () {
       viewModel.setName('New Name');
-      expect(viewModel.project.name, 'New Name');
+      expect(viewModel.project.name, equals('New Name'));
     });
 
     test('setLabelingMode updates mode', () async {
@@ -30,29 +27,40 @@ void main() {
       expect(viewModel.project.mode, LabelingMode.multiClassification);
     });
 
-    test('addClass adds new class if not exists', () {
-      viewModel.addClass('A');
-      expect(viewModel.project.classes.contains('A'), isTrue);
+    test('addClass adds new label class', () {
+      viewModel.addClass('Class A');
+      expect(viewModel.project.classes.contains('Class A'), isTrue);
     });
 
-    test('removeClass removes correct class', () {
-      viewModel.addClass('A');
-      viewModel.addClass('B');
+    test('removeClass removes class by index', () {
+      viewModel.addClass('X');
+      viewModel.addClass('Y');
       viewModel.removeClass(0);
-      expect(viewModel.project.classes.contains('A'), isFalse);
-      expect(viewModel.project.classes.length, 1);
+      expect(viewModel.project.classes, contains('Y'));
+      expect(viewModel.project.classes, isNot(contains('X')));
     });
 
-    test('addDataInfo adds data path', () {
-      viewModel.addDataInfo(DataInfo(fileName: 'test.txt', filePath: '/test'));
+    test('addDataInfo appends dataInfo to list', () {
+      final info = DataInfo(fileName: 'abc.txt', filePath: '/tmp/abc.txt');
+      viewModel.addDataInfo(info);
       expect(viewModel.project.dataInfos.length, 1);
+      expect(viewModel.project.dataInfos.first.fileName, 'abc.txt');
     });
 
-    test('saveProject triggers storage saveProjectConfig', () async {
+    test('saveProject calls repository.saveProject', () async {
       await viewModel.saveProject(true);
-      expect(mockHelper.wasSaveProjectCalled, isTrue);
-      // expect(mockHelper.savedProjects.length, 1);
-      // expect(mockHelper.savedProjects.first.id, equals(initial.id));
+      expect(mockRepository.wasSaveProjectCalled, isTrue);
+    });
+
+    test('deleteProject calls repository.deleteById', () async {
+      viewModel.setName('To Be Deleted');
+      await viewModel.deleteProject();
+      expect(mockRepository.wasDeleteCalled, isTrue);
+    });
+
+    test('clearProjectData calls deleteProjectLabels', () async {
+      await viewModel.clearProjectData();
+      expect(mockRepository.wasLabelDeleted, isTrue);
     });
   });
 }
