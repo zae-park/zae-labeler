0b3e53a7693a16d12f87dedba46630138db5b9c0
zae-park <tom941105@gmail.com>
2025-06-08 04:09:25 +0900

    feat: apply  ProjectIO usecase

--- zae_labeler/lib/src/domain/project/project_use_cases.dart
+++ zae_labeler/lib/src/domain/project/project_use_cases.dart
@@ -1,22 +1,25 @@
+import '../../repositories/project_repository.dart';
+import 'manage_project_io_use_case.dart';
 import 'edit_project_meta_use_case.dart';
 import 'manage_class_list_use_case.dart';
 import 'manage_data_info_use_case.dart';
-import 'save_project_use_case.dart';
 import 'share_project_use_case.dart';
 
 /// 🧩 ViewModel에 주입하기 위한 Project 관련 UseCase 모음
 class ProjectUseCases {
+  final ProjectRepository repository;
   final EditProjectMetaUseCase edit;
   final ManageClassListUseCase classList;
   final ManageDataInfoUseCase dataInfo;
-  final SaveProjectUseCase save;
+  final ManageProjectIOUseCase projectIO;
   final ShareProjectUseCase share;
 
   const ProjectUseCases({
     required this.edit,
     required this.classList,
     required this.dataInfo,
-    required this.save,
+    required this.projectIO,
     required this.share,
+    required this.repository,
   });
 }
