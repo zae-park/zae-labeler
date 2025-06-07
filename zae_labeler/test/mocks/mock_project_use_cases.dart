f3baf5057bf965a07576baf82295d65be7a5d21e
zae-park <tom941105@gmail.com>
2025-06-08 04:31:36 +0900

    test: add initial mock

---
+++ zae_labeler/test/mocks/mock_project_use_cases.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/mockito.dart';
+import 'package:zae_labeler/src/domain/project/project_use_cases.dart';
+
+class MockProjectUseCases extends Mock implements ProjectUseCases {}
+// class MockProjectIOUseCase extends Mock implements ProjectIOUseCase {}
+// class MockSaveProjectUseCase extends Mock implements SaveProjectUseCase {}
