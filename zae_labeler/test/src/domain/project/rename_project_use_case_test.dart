c98e8765969144bf127822894a7e93feb16050ba
zae-park <tom941105@gmail.com>
2025-06-08 03:52:38 +0900

    test: optimize use case

--- zae_labeler/test/src/domain/project/rename_project_use_case_test.dart
+++ zae_labeler/test/src/domain/project/rename_project_use_case_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:zae_labeler/src/domain/project/rename_project_use_case.dart';
+import 'package:zae_labeler/src/domain/project/edit_project_meta_use_case.dart';
 import 'package:zae_labeler/src/models/project_model.dart';
 
 import '../../../mocks/mock_project_repository.dart';
@@ -7,18 +7,18 @@ import '../../../mocks/mock_project_repository.dart';
 void main() {
   group('RenameProjectUseCase', () {
     late MockProjectRepository repository;
-    late RenameProjectUseCase useCase;
+    late EditProjectMetaUseCase useCase;
 
     setUp(() {
       repository = MockProjectRepository();
-      useCase = RenameProjectUseCase(repository: repository);
+      useCase = EditProjectMetaUseCase(repository: repository);
     });
 
     test('updates project name and saves it', () async {
       final project = Project.empty().copyWith(id: 'p1', name: 'Old');
       await repository.saveProject(project);
 
-      await useCase.call('p1', 'NewName');
+      await useCase.rename('p1', 'NewName');
 
       final updated = await repository.findById('p1');
       expect(updated?.name, 'NewName');
