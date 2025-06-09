31c5fec8d15111637542fc802b931a244e3c5afa
zae-park <tom941105@gmail.com>
2025-06-10 01:36:34 +0900

    feat: add LabelUseCases

--- zae_labeler/lib/src/domain/label/label_use_cases.dart
+++ zae_labeler/lib/src/domain/label/label_use_cases.dart
@@ -1,5 +1,6 @@
 import '../../repositories/label_repository.dart';
 import 'single_label_use_case.dart';
+import 'batch_label_use_case.dart';
 import 'validate_label_use_case.dart';
 import 'label_io_use_case.dart';
 
@@ -7,11 +8,17 @@ import 'label_io_use_case.dart';
 class LabelUseCases {
   final LabelRepository repository;
   final SingleLabelUseCases single;
+  final BatchLabelUseCases batch;
   final LabelValidationUseCases validation;
   final LabelIOUseCases io;
 
-  LabelUseCases._({required this.repository, required this.single, required this.validation, required this.io});
+  LabelUseCases._({required this.repository, required this.single, required this.batch, required this.validation, required this.io});
 
   factory LabelUseCases.from(LabelRepository repository) => LabelUseCases._(
-      repository: repository, single: SingleLabelUseCases(repository), validation: LabelValidationUseCases(repository), io: LabelIOUseCases(repository));
+        repository: repository,
+        single: SingleLabelUseCases(repository),
+        batch: BatchLabelUseCases(repository),
+        validation: LabelValidationUseCases(repository),
+        io: LabelIOUseCases(repository),
+      );
 }
