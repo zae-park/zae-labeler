22112decf0fdff678a17d46031c5505ae533d058
zae-park <tom941105@gmail.com>
2025-06-10 01:33:20 +0900

    feat: add LabelUseCases

---
+++ zae_labeler/lib/src/domain/label/label_use_cases.dart
@@ -0,0 +1,17 @@
+import '../../repositories/label_repository.dart';
+import 'single_label_use_case.dart';
+import 'validate_label_use_case.dart';
+import 'label_io_use_case.dart';
+
+/// 🧩 ViewModel에 주입하기 위한 Label 관련 UseCase 모음
+class LabelUseCases {
+  final LabelRepository repository;
+  final SingleLabelUseCases single;
+  final LabelValidationUseCases validation;
+  final LabelIOUseCases io;
+
+  LabelUseCases._({required this.repository, required this.single, required this.validation, required this.io});
+
+  factory LabelUseCases.from(LabelRepository repository) => LabelUseCases._(
+      repository: repository, single: SingleLabelUseCases(repository), validation: LabelValidationUseCases(repository), io: LabelIOUseCases(repository));
+}
