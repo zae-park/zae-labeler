6c45cca93c3f66889117b860947d15b359929969
zae-park <tom941105@gmail.com>
2025-06-10 22:39:20 +0900

    feat: optimize LabelingVMFactory

--- zae_labeler/lib/src/view_models/labeling_view_model.dart
+++ zae_labeler/lib/src/view_models/labeling_view_model.dart
@@ -1,9 +1,9 @@
 import '../models/data_model.dart';
 import '../models/label_model.dart';
 import '../models/project_model.dart';
-import '../utils/adaptive/adaptive_data_loader.dart';
 import '../utils/proxy_storage_helper/interface_storage_helper.dart';
-import '../repositories/label_repository.dart';
+import '../domain/label/label_use_cases.dart';
+import '../utils/adaptive/adaptive_data_loader.dart';
 
 import 'sub_view_models/base_labeling_view_model.dart';
 import 'sub_view_models/classification_labeling_view_model.dart';
@@ -14,28 +14,27 @@ export 'sub_view_models/classification_labeling_view_model.dart';
 export 'sub_view_models/segmentation_labeling_view_model.dart';
 
 /// ✅ LabelingMode에 따른 ViewModel 빌더 매핑
-final Map<LabelingMode, LabelingViewModel Function(Project, StorageHelperInterface, LabelRepository, List<UnifiedData>?)> labelingViewModelBuilders = {
-  LabelingMode.singleClassification: (p, h, r, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, labelRepository: r, initialDataList: dl),
-  LabelingMode.multiClassification: (p, h, r, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, labelRepository: r, initialDataList: dl),
-  LabelingMode.crossClassification: (p, h, r, dl) =>
-      CrossClassificationLabelingViewModel(project: p, storageHelper: h, labelRepository: r, initialDataList: dl),
-  LabelingMode.singleClassSegmentation: (p, h, r, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, labelRepository: r, initialDataList: dl),
-  LabelingMode.multiClassSegmentation: (p, h, r, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, labelRepository: r, initialDataList: dl),
+final Map<LabelingMode, LabelingViewModel Function(Project, StorageHelperInterface, LabelUseCases, List<UnifiedData>?)> labelingViewModelBuilders = {
+  LabelingMode.singleClassification: (p, h, u, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, useCases: u, initialDataList: dl),
+  LabelingMode.multiClassification: (p, h, u, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, useCases: u, initialDataList: dl),
+  LabelingMode.crossClassification: (p, h, u, dl) => CrossClassificationLabelingViewModel(project: p, storageHelper: h, useCases: u, initialDataList: dl),
+  LabelingMode.singleClassSegmentation: (p, h, u, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, useCases: u, initialDataList: dl),
+  LabelingMode.multiClassSegmentation: (p, h, u, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, useCases: u, initialDataList: dl),
 };
 
 /// ✅ LabelingViewModelFactory
 /// - 프로젝트 모드에 따라 적절한 LabelingViewModel을 생성
 class LabelingViewModelFactory {
   /// 동기 생성자
-  static LabelingViewModel create(Project project, StorageHelperInterface helper, LabelRepository labelRepository, {List<UnifiedData>? initialDataList}) {
-    return labelingViewModelBuilders[project.mode]!(project, helper, labelRepository, initialDataList);
+  static LabelingViewModel create(Project project, StorageHelperInterface helper, LabelUseCases useCases, {List<UnifiedData>? initialDataList}) {
+    return labelingViewModelBuilders[project.mode]!(project, helper, useCases, initialDataList);
   }
 
   /// 비동기 생성자
   /// - 플랫폼에 따라 데이터를 로딩하고 초기화까지 수행한 ViewModel 반환
-  static Future<LabelingViewModel> createAsync(Project project, StorageHelperInterface helper, LabelRepository labelRepository) async {
+  static Future<LabelingViewModel> createAsync(Project project, StorageHelperInterface helper, LabelUseCases useCases) async {
     final data = await loadDataAdaptively(project, helper);
-    final vm = labelingViewModelBuilders[project.mode]!(project, helper, labelRepository, data);
+    final vm = labelingViewModelBuilders[project.mode]!(project, helper, useCases, data);
     await vm.initialize();
     return vm;
   }
