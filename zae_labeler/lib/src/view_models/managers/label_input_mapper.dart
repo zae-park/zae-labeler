5985af06b85800b5b840e16a5617f98469bf5509
zae-park <tom941105@gmail.com>
2025-06-15 16:58:35 +0900

    feat: add initial LabelInputMapper

---
+++ zae_labeler/lib/src/view_models/managers/label_input_mapper.dart
@@ -0,0 +1,26 @@
+import '../../models/label_model.dart';
+import '../../models/sub_models/classification_label_model.dart';
+
+abstract class LabelInputMapper {
+  LabelModel map(dynamic labelData, {required String dataId, required String dataPath});
+}
+
+class SingleClassificationInputMapper extends LabelInputMapper {
+  @override
+  LabelModel map(dynamic labelData, {required String dataId, required String dataPath}) {
+    if (labelData != null && labelData is! String) {
+      throw ArgumentError('Expected String or null');
+    }
+    return SingleClassificationLabelModel(dataId: dataId, dataPath: dataPath, labeledAt: DateTime.now(), label: labelData);
+  }
+}
+
+class MultiClassificationInputMapper extends LabelInputMapper {
+  @override
+  LabelModel map(dynamic labelData, {required String dataId, required String dataPath}) {
+    if (labelData is! Set<String>) {
+      throw ArgumentError('Expected Set<String>');
+    }
+    return MultiClassificationLabelModel(dataId: dataId, dataPath: dataPath, labeledAt: DateTime.now(), label: labelData);
+  }
+}
