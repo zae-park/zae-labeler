bbd636cee06d24a94a0f8572786ad541cf46428a
zae-park <tom941105@gmail.com>
2025-06-16 23:09:02 +0900

    test: update MockLabelUseCases using constructor

--- zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
+++ zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
@@ -1,11 +1,18 @@
-// test/mocks/use_cases/mock_label_use_cases.dart
 import 'package:zae_labeler/src/domain/label/label_use_cases.dart';
-import '../../mock_label_repository.dart';
 import '../../mock_storage_helper.dart';
+import 'mock_single_label_use_case.dart';
+import 'mock_batch_label_use_case.dart';
+import 'mock_valid_label_use_case.dart';
+import 'mock_io_label_use_case.dart';
+import '../../mock_label_repository.dart';
 
-class MockLabelUseCases {
-  static LabelUseCases create() {
-    final repo = MockLabelRepository(storageHelper: MockStorageHelper());
-    return LabelUseCases.from(repo);
-  }
+class MockLabelUseCases extends LabelUseCases {
+  MockLabelUseCases()
+      : super(
+          repository: MockLabelRepository(storageHelper: MockStorageHelper()),
+          single: MockSingleLabelUseCases(),
+          batch: MockBatchLabelUseCases(),
+          validation: MockLabelValidationUseCases(),
+          io: MockLabelIOUseCases(storageHelper: MockStorageHelper()),
+        );
 }
