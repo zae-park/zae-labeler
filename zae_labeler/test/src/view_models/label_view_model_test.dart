db0eed96490f1c66fccce37e29811fe7a3ff0f54
zae-park <tom941105@gmail.com>
2025-06-19 01:50:26 +0900

    test: ignore test

--- zae_labeler/test/src/view_models/label_view_model_test.dart
+++ zae_labeler/test/src/view_models/label_view_model_test.dart
@@ -1,71 +1,71 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:zae_labeler/src/domain/label/label_use_cases.dart';
-// import 'package:zae_labeler/src/models/label_model.dart';
-// import 'package:zae_labeler/src/models/sub_models/segmentation_label_model.dart';
-// import 'package:zae_labeler/src/view_models/managers/label_input_mapper.dart';
-// import 'package:zae_labeler/src/view_models/sub_view_models/segmentation_label_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zae_labeler/src/domain/label/label_use_cases.dart';
+import 'package:zae_labeler/src/models/label_model.dart';
+import 'package:zae_labeler/src/models/sub_models/segmentation_label_model.dart';
+import 'package:zae_labeler/src/view_models/managers/label_input_mapper.dart';
+import 'package:zae_labeler/src/view_models/sub_view_models/segmentation_label_view_model.dart';
 
-// import '../../mocks/mock_storage_helper.dart';
-// import '../../mocks/mock_label_repository.dart';
+import '../../mocks/mock_storage_helper.dart';
+import '../../mocks/mock_label_repository.dart';
 
-// void main() {
-//   group('SegmentationLabelViewModel', () {
-//     late SegmentationLabelViewModel labelVM;
-//     late MockLabelRepository mockLabelRepo;
-//     late MockStorageHelper mockStorage;
+void main() {
+  group('SegmentationLabelViewModel', () {
+    late SegmentationLabelViewModel labelVM;
+    late MockLabelRepository mockLabelRepo;
+    late MockStorageHelper mockStorage;
 
-//     setUp(() {
-//       mockStorage = MockStorageHelper();
-//       mockLabelRepo = MockLabelRepository(storageHelper: mockStorage);
+    setUp(() {
+      mockStorage = MockStorageHelper();
+      mockLabelRepo = MockLabelRepository(storageHelper: mockStorage);
 
-//       labelVM = SegmentationLabelViewModel(
-//         projectId: 'proj-123',
-//         dataId: 'data-001',
-//         dataFilename: 'image.png',
-//         dataPath: '/path/image.png',
-//         mode: LabelingMode.multiClassSegmentation,
-//         labelModel: MultiClassSegmentationLabelModel.empty(),
-//         labelUseCases: LabelUseCases.from(mockLabelRepo),
-//         labelInputMapper: MultiSegmentationInputMapper(),
-//       );
-//     });
+      labelVM = SegmentationLabelViewModel(
+        projectId: 'proj-123',
+        dataId: 'data-001',
+        dataFilename: 'image.png',
+        dataPath: '/path/image.png',
+        mode: LabelingMode.multiClassSegmentation,
+        labelModel: MultiClassSegmentationLabelModel.empty(),
+        labelUseCases: LabelUseCases.from(mockLabelRepo),
+        labelInputMapper: MultiSegmentationInputMapper(),
+      );
+    });
 
-//     test('initial label is empty', () {
-//       final label = labelVM.labelModel.label as SegmentationData;
-//       expect(label.segments, isEmpty);
-//     });
+  //   test('initial label is empty', () {
+  //     final label = labelVM.labelModel.label as SegmentationData;
+  //     expect(label.segments, isEmpty);
+  //   });
 
-//     test('addPixel adds pixel to the correct class', () {
-//       labelVM.addPixel(10, 20, 'tree');
-//       final label = labelVM.labelModel.label as SegmentationData;
+  //   test('addPixel adds pixel to the correct class', () {
+  //     labelVM.addPixel(10, 20, 'tree');
+  //     final label = labelVM.labelModel.label as SegmentationData;
 
-//       expect(label.segments.containsKey('tree'), isTrue);
-//       expect(label.segments['tree']!.indices.contains((10, 20)), isTrue);
-//     });
+  //     expect(label.segments.containsKey('tree'), isTrue);
+  //     expect(label.segments['tree']!.indices.contains((10, 20)), isTrue);
+  //   });
 
-//     test('removePixel removes pixel from the correct class', () {
-//       labelVM.addPixel(5, 5, 'sky');
-//       labelVM.removePixel(5, 5);
+  //   test('removePixel removes pixel from the correct class', () {
+  //     labelVM.addPixel(5, 5, 'sky');
+  //     labelVM.removePixel(5, 5);
 
-//       final label = labelVM.labelModel.label as SegmentationData;
-//       expect(label.segments['sky'], isNull);
-//     });
+  //     final label = labelVM.labelModel.label as SegmentationData;
+  //     expect(label.segments['sky'], isNull);
+  //   });
 
-//     test('updateLabel replaces the labelModel content', () async {
-//       final newLabel = SegmentationData(segments: {
-//         'road': Segment(indices: {(1, 2), (3, 4)}, classLabel: 'road'),
-//       });
+  //   test('updateLabel replaces the labelModel content', () async {
+  //     final newLabel = SegmentationData(segments: {
+  //       'road': Segment(indices: {(1, 2), (3, 4)}, classLabel: 'road'),
+  //     });
 
-//       final newLabelModel = labelVM.labelInputMapper.map(newLabel, dataId: "data-001", dataPath: "/path/image.png");
-//       await labelVM.updateLabel(newLabelModel);
+  //     final newLabelModel = labelVM.labelInputMapper.map(newLabel, dataId: "data-001", dataPath: "/path/image.png");
+  //     await labelVM.updateLabel(newLabelModel);
 
-//       final label = labelVM.labelModel.label as SegmentationData;
-//       expect(label.segments['road']?.indices.length, equals(2));
-//       expect(label.segments['road']!.indices.contains((3, 4)), isTrue);
-//     });
+  //     final label = labelVM.labelModel.label as SegmentationData;
+  //     expect(label.segments['road']?.indices.length, equals(2));
+  //     expect(label.segments['road']!.indices.contains((3, 4)), isTrue);
+  //   });
 
-//     // test('updateLabel throws error on invalid data', () {
-//     //   expect(() => labelVM.updateLabel('invalid'), throwsA(isA<ArgumentError>()));
-//     // });
-//   });
-// }
+  //   // test('updateLabel throws error on invalid data', () {
+  //   //   expect(() => labelVM.updateLabel('invalid'), throwsA(isA<ArgumentError>()));
+  //   // });
+  // });
+}
--- zae_labeler/test/src/view_models/project_view_model_test.dart
+++ zae_labeler/test/src/view_models/project_view_model_test.dart
@@ -1,67 +1,67 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:zae_labeler/src/view_models/project_view_model.dart';
-// import 'package:zae_labeler/src/models/label_model.dart';
-// import 'package:zae_labeler/src/models/data_model.dart';
-// import '../../mocks/mock_share_helper.dart';
-// import '../../mocks/use_cases/project/mock_project_use_cases.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zae_labeler/src/view_models/project_view_model.dart';
+import 'package:zae_labeler/src/models/label_model.dart';
+import 'package:zae_labeler/src/models/data_model.dart';
+import '../../mocks/mock_share_helper.dart';
+import '../../mocks/use_cases/project/mock_project_use_cases.dart';
 
-// void main() {
-//   group('ProjectViewModel (Refactored)', () {
-//     late ProjectViewModel viewModel;
-//     late MockProjectUseCases mockUseCases;
-//     late MockShareHelper mockShare;
+void main() {
+  group('ProjectViewModel (Refactored)', () {
+    late ProjectViewModel viewModel;
+    late MockProjectUseCases mockUseCases;
+    late MockShareHelper mockShare;
 
-//     setUp(() {
-//       mockShare = MockShareHelper();
-//       mockUseCases = MockProjectUseCases();
-//       viewModel = ProjectViewModel(useCases: mockUseCases, shareHelper: mockShare);
-//     });
+    setUp(() {
+      mockShare = MockShareHelper();
+      mockUseCases = MockProjectUseCases();
+      viewModel = ProjectViewModel(useCases: mockUseCases, shareHelper: mockShare);
+    });
 
-//     test('setName updates project name', () async {
-//       await viewModel.useCases.edit.rename(viewModel.project.id, 'New Name');
-//       expect(viewModel.project.name, equals(''));
-//     });
+    // test('setName updates project name', () async {
+    //   await viewModel.useCases.edit.rename(viewModel.project.id, 'New Name');
+    //   expect(viewModel.project.name, equals(''));
+    // });
 
-//     test('setLabelingMode updates mode', () async {
-//       await viewModel.useCases.edit.changeLabelingMode(viewModel.project.id, LabelingMode.multiClassification);
-//       expect(viewModel.project.mode, LabelingMode.singleClassification);
-//     });
+    // test('setLabelingMode updates mode', () async {
+    //   await viewModel.useCases.edit.changeLabelingMode(viewModel.project.id, LabelingMode.multiClassification);
+    //   expect(viewModel.project.mode, LabelingMode.singleClassification);
+    // });
 
-//     // test('addClass adds new label class', () {
-//     //   viewModel.addClass('Class A');
-//     //   expect(viewModel.project.classes.contains('Class A'), isTrue);
-//     // });
+    // test('addClass adds new label class', () {
+    //   viewModel.addClass('Class A');
+    //   expect(viewModel.project.classes.contains('Class A'), isTrue);
+    // });
 
-//     // test('removeClass removes class by index', () async {
-//     //   await viewModel.addClass('X');
-//     //   await viewModel.addClass('Y');
-//     //   await viewModel.removeClass(0);
-//     //   expect(viewModel.project.classes, isNot(contains('Y')));
-//     //   expect(viewModel.project.classes, isNot(contains('X')));
-//     // });
+    // test('removeClass removes class by index', () async {
+    //   await viewModel.addClass('X');
+    //   await viewModel.addClass('Y');
+    //   await viewModel.removeClass(0);
+    //   expect(viewModel.project.classes, isNot(contains('Y')));
+    //   expect(viewModel.project.classes, isNot(contains('X')));
+    // });
 
-//     // test('addDataInfo appends dataInfo to list', () async {
-//     //   final info = DataInfo(fileName: 'abc.txt', filePath: '/tmp/abc.txt');
-//     //   await viewModel.addDataInfo(info);
-//     //   expect(viewModel.project.dataInfos.length, 0);
-//     //   // expect(viewModel.project.dataInfos.first.fileName, 'abc.txt');
-//     // });
+    // test('addDataInfo appends dataInfo to list', () async {
+    //   final info = DataInfo(fileName: 'abc.txt', filePath: '/tmp/abc.txt');
+    //   await viewModel.addDataInfo(info);
+    //   expect(viewModel.project.dataInfos.length, 0);
+    //   // expect(viewModel.project.dataInfos.first.fileName, 'abc.txt');
+    // });
 
-//     test('saveProject calls repository.saveProject', () async {
-//       expect(() async => await viewModel.saveProject(true), throwsA(isA<ArgumentError>()));
+    // test('saveProject calls repository.saveProject', () async {
+    //   expect(() async => await viewModel.saveProject(true), throwsA(isA<ArgumentError>()));
 
-//       // expect(mockRepository.wasSaveProjectCalled, isTrue);
-//     });
+    //   // expect(mockRepository.wasSaveProjectCalled, isTrue);
+    // });
 
-//     // test('deleteProject calls repository.deleteById', () async {
-//     //   viewModel.setName('To Be Deleted');
-//     //   await viewModel.deleteProject();
-//     //   expect(mockRepository.wasDeleteCalled, isTrue);
-//     // });
+    // test('deleteProject calls repository.deleteById', () async {
+    //   viewModel.setName('To Be Deleted');
+    //   await viewModel.deleteProject();
+    //   expect(mockRepository.wasDeleteCalled, isTrue);
+    // });
 
-//     // test('clearProjectData calls deleteProjectLabels', () async {
-//     //   await viewModel.clearProjectLabels();
-//     //   // expect(mockRepository.wasLabelDeleted, isTrue);
-//     // });
-//   });
-// }
+    // test('clearProjectData calls deleteProjectLabels', () async {
+    //   await viewModel.clearProjectLabels();
+    //   // expect(mockRepository.wasLabelDeleted, isTrue);
+    // });
+  });
+}
