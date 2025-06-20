cf7c3808b4f40f793c59da6544f82f71d0c47e9d
zae-park <tom941105@gmail.com>
2025-06-21 01:19:20 +0900

    test: add Call Flag to Mock

--- zae_labeler/test/mocks/helpers/mock_storage_helper.dart
+++ zae_labeler/test/mocks/helpers/mock_storage_helper.dart
@@ -8,9 +8,11 @@ import 'package:zae_labeler/src/models/data_model.dart';
 class MockStorageHelper implements StorageHelperInterface {
   final Map<String, Map<String, LabelModel>> _labelStorage = {}; // 📦 label 저장소
   List<Project> savedProjects = [];
-  bool wasSaveProjectCalled = false;
-  bool shouldThrowOnSave = false;
   Project? mockImportedProject;
+
+  bool wasSaveProjectCalled = false;
+  bool wasClearCacheCalled = false;
+  bool shouldThrowOnSave = false;
 
   @override
   Future<void> saveProjectConfig(List<Project> project) async {
@@ -89,7 +91,9 @@ class MockStorageHelper implements StorageHelperInterface {
   Future<List<LabelModel>> importAllLabels() async => [];
 
   @override
-  Future<void> clearAllCache() async {}
+  Future<void> clearAllCache() async {
+    wasClearCacheCalled = true;
+  }
 
   @override
   Future<void> deleteProject(String projectId) async {
