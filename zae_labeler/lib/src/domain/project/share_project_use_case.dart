e251cc07ac711034a0961cb71f2a4dcfe1b83247
zae-park <tom941105@gmail.com>
2025-06-21 01:23:00 +0900

    feat: remove injection

--- zae_labeler/lib/src/domain/project/share_project_use_case.dart
+++ zae_labeler/lib/src/domain/project/share_project_use_case.dart
@@ -4,16 +4,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../../models/project_model.dart';
 import '../../repositories/project_repository.dart';
+import '../../utils/proxy_share_helper/interface_share_helper.dart';
 import '../../utils/share_helper.dart';
 import '../validator/project_validator.dart';
 
 /// ✅ UseCase: 프로젝트 공유
 /// - JSON으로 직렬화한 프로젝트를 플랫폼별 공유 방식으로 전달
 class ShareProjectUseCase {
-  final shareHelper = getShareHelper();
+  final ShareHelperInterface shareHelper;
   final ProjectRepository repository;
 
-  ShareProjectUseCase({required this.repository});
+  ShareProjectUseCase({required this.repository, ShareHelperInterface? shareHelper}) : shareHelper = shareHelper ?? getShareHelper();
 
   Future<void> call(BuildContext context, Project project) async {
     ProjectValidator.validate(project);
