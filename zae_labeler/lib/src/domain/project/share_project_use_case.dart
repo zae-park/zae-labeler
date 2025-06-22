aef21cae7237fc33a6cd3b8c74088d4917f74d8f
zae-park <tom941105@gmail.com>
2025-06-22 17:47:31 +0900

    feat: add protect filed to injection

--- zae_labeler/lib/src/domain/project/share_project_use_case.dart
+++ zae_labeler/lib/src/domain/project/share_project_use_case.dart
@@ -11,20 +11,20 @@ import '../validator/project_validator.dart';
 /// ✅ UseCase: 프로젝트 공유
 /// - JSON으로 직렬화한 프로젝트를 플랫폼별 공유 방식으로 전달
 class ShareProjectUseCase {
-  final ShareHelperInterface shareHelper;
+  ShareHelperInterface _shareHelper;
   final ProjectRepository repository;
 
   set shareHelper(ShareHelperInterface helper) {
-    shareHelper = helper;
+    _shareHelper = helper;
   }
 
-  ShareProjectUseCase({required this.repository, ShareHelperInterface? shareHelper}) : shareHelper = shareHelper ?? getShareHelper();
+  ShareProjectUseCase({required this.repository, ShareHelperInterface? shareHelper}) : _shareHelper = shareHelper ?? getShareHelper();
 
   Future<void> call(BuildContext context, Project project) async {
     ProjectValidator.validate(project);
     final jsonString = jsonEncode(project.toJson());
 
     final filePath = await repository.exportConfig(project);
-    await shareHelper.shareProject(name: project.name, jsonString: jsonString, getFilePath: () async => filePath);
+    await _shareHelper.shareProject(name: project.name, jsonString: jsonString, getFilePath: () async => filePath);
   }
 }
