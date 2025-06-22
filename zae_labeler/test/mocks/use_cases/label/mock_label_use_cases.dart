4cace58dfae0c84144bc31d9d6d20367f650e5f2
zae-park <tom941105@gmail.com>
2025-06-22 19:07:14 +0900

    test: update MockUseCase

--- zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
+++ zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
@@ -1,18 +1,18 @@
 import 'package:zae_labeler/src/domain/label/label_use_cases.dart';
-import '../../mock_storage_helper.dart';
+import '../../repositories/mock_label_repository.dart';
 import 'mock_single_label_use_case.dart';
 import 'mock_batch_label_use_case.dart';
 import 'mock_valid_label_use_case.dart';
 import 'mock_io_label_use_case.dart';
-import '../../mock_label_repository.dart';
 
 class MockLabelUseCases extends LabelUseCases {
+  static final _repo = MockLabelRepository();
   MockLabelUseCases()
       : super(
-          repository: MockLabelRepository(storageHelper: MockStorageHelper()),
-          single: MockSingleLabelUseCases(),
-          batch: MockBatchLabelUseCases(),
-          validation: MockLabelValidationUseCases(),
-          io: MockLabelIOUseCases(storageHelper: MockStorageHelper()),
+          repository: _repo,
+          single: MockSingleLabelUseCase(repository: _repo),
+          batch: MockBatchLabelUseCase(repository: _repo),
+          validation: MockLabelValidationUseCase(repository: _repo),
+          io: MockLabelIOUseCase(repository: _repo),
         );
 }
