3235bcb819170d7c8d7116ba24660e454a70dee1
zae-park <tom941105@gmail.com>
2025-06-22 17:55:09 +0900

    test: update MockHelper

--- zae_labeler/test/mocks/helpers/mock_share_helper.dart
+++ zae_labeler/test/mocks/helpers/mock_share_helper.dart
@@ -1,14 +1,17 @@
 import 'package:zae_labeler/src/utils/proxy_share_helper/interface_share_helper.dart';
 
+/// 🧪 테스트용 MockShareHelper
 class MockShareHelper implements ShareHelperInterface {
   bool wasCalled = false;
   String? sharedName;
   String? sharedJson;
+  late Future<String> Function() sharedFilePathGetter;
 
   @override
   Future<void> shareProject({required String name, required String jsonString, required Future<String> Function() getFilePath}) async {
     wasCalled = true;
     sharedName = name;
     sharedJson = jsonString;
+    sharedFilePathGetter = getFilePath;
   }
 }
