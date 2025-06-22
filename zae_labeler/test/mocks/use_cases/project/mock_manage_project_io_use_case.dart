416a9f9eaa0d6268ffd3a86aac4a29e410e42075
zae-park <tom941105@gmail.com>
2025-06-22 18:01:33 +0900

    test: update MockUseCases

--- zae_labeler/test/mocks/use_cases/project/mock_manage_project_io_use_case.dart
+++ zae_labeler/test/mocks/use_cases/project/mock_manage_project_io_use_case.dart
@@ -1,10 +1,56 @@
+import 'package:zae_labeler/src/models/project_model.dart';
 import 'package:zae_labeler/src/domain/project/manage_project_io_use_case.dart';
-import '../../mock_project_repository.dart';
 
 class MockManageProjectIOUseCase extends ManageProjectIOUseCase {
-  MockManageProjectIOUseCase() : super(repository: MockProjectRepository());
+  List<Project> savedProjects = [];
+  List<String> deletedIds = [];
+  List<Project> importedProjects = [];
+  String exportedPath = '';
+  bool wasClearCacheCalled = false;
 
-  Future<void> saveProject(String projectId) async {}
+  MockManageProjectIOUseCase({required super.repository});
 
-  Future<void> deleteProject(String projectId) async {}
+  @override
+  Future<void> saveOne(Project project) async {
+    savedProjects.removeWhere((p) => p.id == project.id);
+    savedProjects.add(project);
+  }
+
+  @override
+  Future<void> saveAll(List<Project> projects) async {
+    savedProjects = projects;
+  }
+
+  @override
+  Future<void> deleteById(String projectId) async {
+    deletedIds.add(projectId);
+    savedProjects.removeWhere((p) => p.id == projectId);
+  }
+
+  @override
+  Future<void> deleteAll(List<String> projectIds) async {
+    deletedIds.addAll(projectIds);
+    savedProjects.removeWhere((p) => projectIds.contains(p.id));
+  }
+
+  @override
+  Future<List<Project>> fetchAll() async {
+    return savedProjects;
+  }
+
+  @override
+  Future<void> clearCache() async {
+    wasClearCacheCalled = true;
+  }
+
+  @override
+  Future<List<Project>> importProjects() async {
+    return importedProjects;
+  }
+
+  @override
+  Future<String> exportProject(Project project) async {
+    exportedPath = '/mock/export/${project.id}.json';
+    return exportedPath;
+  }
 }
--- zae_labeler/test/mocks/use_cases/project/mock_project_use_cases.dart
+++ zae_labeler/test/mocks/use_cases/project/mock_project_use_cases.dart
@@ -1,20 +1,20 @@
-// test/mocks/use_cases/project/mock_project_use_cases.dart
 import 'package:zae_labeler/src/domain/project/project_use_cases.dart';
-import '../../mock_project_repository.dart';
+import '../../repositories/mock_project_repository.dart';
 import 'mock_edit_project_use_case.dart';
-import 'mock_io_project_use_case.dart';
+import 'mock_manage_project_io_use_case.dart';
 import 'mock_manage_class_list_use_case.dart';
 import 'mock_manage_data_info_use_case.dart';
 import 'mock_share_project_use_case.dart';
 
 class MockProjectUseCases extends ProjectUseCases {
+  static final _repo = MockProjectRepository();
   MockProjectUseCases()
       : super(
-          repository: MockProjectRepository(),
-          edit: MockEditProjectMetaUseCase(),
-          classList: MockManageClassListUseCase(),
-          dataInfo: MockManageDataInfoUseCase(),
-          io: MockManageProjectIOUseCase(),
-          share: MockShareProjectUseCase(),
+          repository: _repo,
+          edit: MockEditProjectMetaUseCase(repository: _repo),
+          classList: MockManageClassListUseCase(repository: _repo),
+          dataInfo: MockManageDataInfoUseCase(repository: _repo),
+          io: MockManageProjectIOUseCase(repository: _repo),
+          share: MockShareProjectUseCase(repository: _repo),
         );
 }
