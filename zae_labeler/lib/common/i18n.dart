03b183553efc09187234049843b70a59d810a9c9
zae-park <tom941105@gmail.com>
2025-06-26 01:55:13 +0900

    feat: fix l10n

--- zae_labeler/lib/common/i18n.dart
+++ zae_labeler/lib/common/i18n.dart
@@ -1,6 +1,6 @@
 // 📁 lib/common/i18n.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../l10n/app_localizations.dart';
 
 extension L10nExt on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
