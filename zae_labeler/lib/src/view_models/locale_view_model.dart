9815617a448e6e9429d4710e5e0bb55872106540
zae-park <tom941105@gmail.com>
2025-07-07 23:00:25 +0900

    remove dummy

--- zae_labeler/lib/src/view_models/locale_view_model.dart
+++ zae_labeler/lib/src/view_models/locale_view_model.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:zae_labeler/src/core/services/user_preference_service.dart';
+// import 'package:flutter/material.dart';
+// import 'package:zae_labeler/src/core/services/user_preference_service.dart';
 
-class LocaleViewModel extends ChangeNotifier {
-  Locale _currentLocale;
-  final UserPreferenceService _preferenceService;
+// class LocaleViewModel extends ChangeNotifier {
+//   Locale _currentLocale;
+//   final UserPreferenceService _preferenceService;
 
-  Locale get currentLocale => _currentLocale;
+//   Locale get currentLocale => _currentLocale;
 
-  /// ✅ 생성 시 저장된 로케일을 초기화하거나 주입된 값 사용
-  LocaleViewModel({required UserPreferenceService preferenceService, Locale? initial})
-      : _preferenceService = preferenceService,
-        _currentLocale = initial ?? preferenceService.locale;
+//   /// ✅ 생성 시 저장된 로케일을 초기화하거나 주입된 값 사용
+//   LocaleViewModel({required UserPreferenceService preferenceService, Locale? initial})
+//       : _preferenceService = preferenceService,
+//         _currentLocale = initial ?? preferenceService.locale;
 
-  /// ✅ 로케일 변경 시 SharedPreferences에 저장까지 수행
-  Future<void> changeLocale(String languageCode) async {
-    _currentLocale = Locale(languageCode);
-    notifyListeners();
-    await _preferenceService.setLocale(_currentLocale);
-  }
-}
+//   /// ✅ 로케일 변경 시 SharedPreferences에 저장까지 수행
+//   Future<void> changeLocale(String languageCode) async {
+//     _currentLocale = Locale(languageCode);
+//     notifyListeners();
+//     await _preferenceService.setLocale(_currentLocale);
+//   }
+// }
