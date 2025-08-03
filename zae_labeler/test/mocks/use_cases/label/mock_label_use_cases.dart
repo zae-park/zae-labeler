e9bce921762415fcc94bb953634655cd5835c45b
zae-park <tom941105@gmail.com>
2025-08-03 12:31:53 +0900

    test: update mock

--- zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
+++ zae_labeler/test/mocks/use_cases/label/mock_label_use_cases.dart
@@ -1,7 +1,7 @@
 import 'package:zae_labeler/src/features/label/use_cases/label_use_cases.dart';
 import '../../repositories/mock_label_repository.dart';
-import 'mock_single_label_use_case.dart';
-import 'mock_batch_label_use_case.dart';
+// import 'mock_single_label_use_case.dart';
+// import 'mock_batch_label_use_case.dart';
 import 'mock_label_summary_use_case.dart';
 import 'mock_valid_label_use_case.dart';
 import 'mock_io_label_use_case.dart';
@@ -12,8 +12,6 @@ class MockLabelUseCases extends LabelUseCases {
   MockLabelUseCases._with(MockLabelRepository repo)
       : super(
           repository: repo,
-          single: MockSingleLabelUseCase(repository: repo),
-          batch: MockBatchLabelUseCase(repository: repo),
           validation: _validation,
           io: MockLabelIOUseCase(repository: repo),
           summary: MockLabelSummaryUseCase(repository: repo, validUseCase: _validation),
