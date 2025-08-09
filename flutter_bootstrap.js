4333043c78f159e2b9ef2d967a2052abbccb19d5
zae-park <zae-park@users.noreply.github.com>
2025-08-09 13:41:12 +0000

    deploy: 37822b8bb3ab85bed062e2a1a3024c5ef6af3d72

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2833165347"
+    serviceWorkerVersion: "3484279890"
   }
 });
