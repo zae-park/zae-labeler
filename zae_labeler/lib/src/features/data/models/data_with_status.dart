59fe089293e40e452a0defefab1ad803fd72875d
zae-park <tom941105@gmail.com>
2025-08-16 02:23:43 +0900

    feat: add initial DataModel for UI

---
+++ zae_labeler/lib/src/features/data/models/data_with_status.dart
@@ -0,0 +1,10 @@
+// lib/src/features/data/models/data_with_status.dart
+import 'package:zae_labeler/src/core/models/data/unified_data.dart';
+import 'package:zae_labeler/src/features/label/models/label_model.dart'; // LabelStatus
+
+class DataWithStatus {
+  final UnifiedData data;
+  final LabelStatus status;
+
+  const DataWithStatus({required this.data, required this.status});
+}
