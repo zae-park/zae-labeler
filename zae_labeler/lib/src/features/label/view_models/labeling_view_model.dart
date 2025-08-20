111b95357a7d7d7fbd8666fdbe5e2cbfd86e56d6
zae-park <tom941105@gmail.com>
2025-08-20 14:41:14 +0900

    feat: update labeingVM builder

--- zae_labeler/lib/src/features/label/view_models/labeling_view_model.dart
+++ zae_labeler/lib/src/features/label/view_models/labeling_view_model.dart
@@ -1,7 +1,8 @@
-import 'package:zae_labeler/src/features/data/models/data_with_status.dart';
-import 'package:zae_labeler/src/features/data/services/adaptive_unified_data_loader.dart';
+// lib/src/features/label/view_models/labeling_view_model.dart
+import 'package:zae_labeler/src/core/models/data/unified_data.dart';
+import 'package:zae_labeler/src/features/data/services/adaptive_unified_data_loader.dart' show loadDataAdaptively;
 
-import '../models/label_model.dart';
+import '../models/label_model.dart' show LabelingMode;
 import '../../../core/models/project/project_model.dart';
 import '../../../platform_helpers/storage/interface_storage_helper.dart';
 import '../../../core/use_cases/app_use_cases.dart';
@@ -10,32 +11,30 @@ import 'sub_view_models/base_labeling_view_model.dart';
 import 'sub_view_models/classification_labeling_view_model.dart';
 import 'sub_view_models/segmentation_labeling_view_model.dart';
 
-export 'sub_view_models/base_labeling_view_model.dart';
-export 'sub_view_models/classification_labeling_view_model.dart';
-export 'sub_view_models/segmentation_labeling_view_model.dart';
+typedef VMBuilder = LabelingViewModel Function(Project project, StorageHelperInterface storage, AppUseCases appUseCases, List<UnifiedData>? initialData);
 
-/// ✅ LabelingMode에 따른 ViewModel 빌더 매핑
-final Map<LabelingMode, LabelingViewModel Function(Project, StorageHelperInterface, AppUseCases, List<DataWithStatus>?)> labelingViewModelBuilders = {
-  LabelingMode.singleClassification: (p, h, u, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, appUseCases: u, initialDataList: dl),
-  LabelingMode.multiClassification: (p, h, u, dl) => ClassificationLabelingViewModel(project: p, storageHelper: h, appUseCases: u, initialDataList: dl),
-  LabelingMode.crossClassification: (p, h, u, dl) => CrossClassificationLabelingViewModel(project: p, storageHelper: h, appUseCases: u, initialDataList: dl),
-  LabelingMode.singleClassSegmentation: (p, h, u, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, appUseCases: u, initialDataList: dl),
-  LabelingMode.multiClassSegmentation: (p, h, u, dl) => SegmentationLabelingViewModel(project: p, storageHelper: h, appUseCases: u, initialDataList: dl),
+/// ✅ LabelingMode → VM 빌더 매핑 (initialData: List<UnifiedData>?)
+final Map<LabelingMode, VMBuilder> labelingViewModelBuilders = {
+  LabelingMode.singleClassification: (p, s, u, d) => ClassificationLabelingViewModel(project: p, storageHelper: s, appUseCases: u, initialDataList: d),
+  LabelingMode.multiClassification: (p, s, u, d) => ClassificationLabelingViewModel(project: p, storageHelper: s, appUseCases: u, initialDataList: d),
+  LabelingMode.crossClassification: (p, s, u, d) => CrossClassificationLabelingViewModel(project: p, storageHelper: s, appUseCases: u, initialDataList: d),
+  LabelingMode.singleClassSegmentation: (p, s, u, d) => SegmentationLabelingViewModel(project: p, storageHelper: s, appUseCases: u, initialDataList: d),
+  LabelingMode.multiClassSegmentation: (p, s, u, d) => SegmentationLabelingViewModel(project: p, storageHelper: s, appUseCases: u, initialDataList: d),
 };
 
 /// ✅ LabelingViewModelFactory
-/// - 프로젝트 모드에 따라 적절한 LabelingViewModel을 생성
+/// - 프로젝트 모드에 맞는 VM 생성
 class LabelingViewModelFactory {
-  /// 동기 생성자
-  static LabelingViewModel create(Project project, StorageHelperInterface helper, AppUseCases useCases, {List<DataWithStatus>? initialDWSList}) {
-    return labelingViewModelBuilders[project.mode]!(project, helper, useCases, initialDWSList);
+  /// 동기 생성
+  static LabelingViewModel create(Project project, StorageHelperInterface storage, AppUseCases useCases, {List<UnifiedData>? initialData}) {
+    final builder = labelingViewModelBuilders[project.mode]!;
+    return builder(project, storage, useCases, initialData);
   }
 
-  /// 비동기 생성자
-  /// - 플랫폼에 따라 데이터를 로딩하고 초기화까지 수행한 ViewModel 반환
-  static Future<LabelingViewModel> createAsync(Project project, StorageHelperInterface helper, AppUseCases useCases) async {
-    final data = await loadDataAdaptively(project, helper);
-    final vm = labelingViewModelBuilders[project.mode]!(project, helper, useCases, data);
+  /// 비동기 생성: 데이터 로드 + VM 초기화
+  static Future<LabelingViewModel> createAsync(Project project, StorageHelperInterface storage, AppUseCases useCases) async {
+    final data = await loadDataAdaptively(project, storage); // List<UnifiedData>
+    final vm = create(project, storage, useCases, initialData: data);
     await vm.initialize();
     return vm;
   }
