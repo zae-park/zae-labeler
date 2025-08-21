9d52d1e305ce4774f048df37e02825f3873cb135
zae-park <tom941105@gmail.com>
2025-08-22 03:36:58 +0900

    test: remove legacy test cases

--- zae_labeler/test/mocks/use_cases/label/mock_label_summary_use_case.dart
+++
@@ -1,9 +0,0 @@
-import 'package:zae_labeler/src/features/label/use_cases/labeling_summary_use_case.dart';
-import 'package:zae_labeler/src/core/models/project/project_model.dart';
-
-class MockLabelSummaryUseCase extends LabelingSummaryUseCase {
-  MockLabelSummaryUseCase({required super.repository, required super.validUseCase});
-
-  @override
-  Future<LabelingSummary> load(Project project) async => LabelingSummary.dummy();
-}
