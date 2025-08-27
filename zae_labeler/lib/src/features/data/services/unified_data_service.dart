12960d7609ae23fd3b1f88f1ca439c0c26a0f0e4
zae-park <tom941105@gmail.com>
2025-08-28 00:21:33 +0900

    feat: add debug print

--- zae_labeler/lib/src/features/data/services/unified_data_service.dart
+++ zae_labeler/lib/src/features/data/services/unified_data_service.dart
@@ -1,4 +1,5 @@
 // lib/src/features/data/services/unified_data_service.dart
+import 'package:flutter/foundation.dart';
 import 'package:zae_labeler/src/core/models/data/file_type.dart';
 import 'package:zae_labeler/src/core/models/data/unified_data.dart';
 import 'package:zae_labeler/src/core/models/data/data_info.dart';
@@ -11,6 +12,7 @@ class UnifiedDataService {
   final DataLoader _loader = createDataLoader();
 
   Future<UnifiedData> fromDataInfo(DataInfo info) {
+    debugPrint('[UniDataService] id=${info.id} name=${info.fileName} path=${info.filePath} mime=${info.mimeType}');
     // 필요시 여기서 추가 전처리/후처리(파싱 규칙 등)를 넣을 수 있습니다.
     return _loader.fromDataInfo(info);
   }
@@ -18,6 +20,9 @@ class UnifiedDataService {
   /// 빈 데이터(플레이스홀더) 필요 시
   UnifiedData empty() {
     // 프로젝트에 맞게 조정
-    return UnifiedData(dataInfo: DataInfo.create(fileName: 'untitled'), fileType: FileType.unsupported);
+    return UnifiedData(
+      dataInfo: DataInfo.create(fileName: 'untitled'),
+      fileType: FileType.unsupported,
+    );
   }
 }
